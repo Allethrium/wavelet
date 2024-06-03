7c49613605349e0d01745034d2e61a5b91402b2e
armelvil <103518994+armelvil@users.noreply.github.com>
2024-06-03 15:27:21 -0400

    Move packages around, add upgrade step to ensure package versions are good
    - changed oneVPL to libvpl
    - added metadata refresh for rpm-ostree

--- webfiles/root/usr/local/bin/wavelet_installer_xf.sh
+++ webfiles/root/usr/local/bin/wavelet_installer_xf.sh
@@ -40,6 +40,8 @@ event_server(){
 	mkdir -p /home/wavelet/.config/containers/systemd/
 	chown -R wavelet:wavelet /home/wavelet
 	cd /home/wavelet
+	/usr/bin/rpm-ostree refresh-md
+	/usr/bin/rpm-ostree upgrade
 	rpm_ostree_install_git
 	git clone https://github.com/ALLETHRIUM/wavelet
 	generate_tarfiles
@@ -77,6 +79,7 @@ sleep 2
 -y -A \
 https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+/usr/bin/rpm-ostree refresh-md
 echo -e "RPM Fusion repo installed, waiting for 2 seconds..\n"
 sleep 2
 
@@ -86,10 +89,9 @@ sleep 2
 -y -A --idempotent \
 intel-media-driver \
 intel-gpu-tools intel-compute-runtime oneVPL-intel-gpu intel-media-driver intel-gmmlib \
-intel-level-zero oneapi-level-zero oneVPL intel-mediasdk libva libva-utils libva-v4l2-request libva-vdpau-driver intel-ocloc \
+intel-level-zero oneapi-level-zero libvpl intel-mediasdk libva libva-utils libva-v4l2-request libva-vdpau-driver intel-ocloc \
 ocl-icd opencl-headers mpv libsrtp mesa-dri-drivers intel-opencl \
-libvdpau-va-gl mesa-vdpau-drivers libvdpau libvdpau-devel \
-libva-intel-driver \
+libvdpau-va-gl mesa-vdpau-drivers libvdpau libvdpau-devel libva-intel-driver \
 ffmpeg ffmpeg-libs libheif-freeworld \
 neofetch htop \
 mesa-libOpenCL python3-pip srt srt-libs ffmpeg vlc libv4l v4l-utils libva-v4l2-request pipewire-v4l2 \
