cd4abbdd53770f95b450bbfef8052f2ee0f987d2
armelvil <103518994+armelvil@users.noreply.github.com>
2024-06-20 17:49:42 -0400

    fix inotify args so they.. you know, work.

--- webfiles/root/usr/local/bin/wavelet_dnsmasq_inotify_service.sh
+++ webfiles/root/usr/local/bin/wavelet_dnsmasq_inotify_service.sh
@@ -2,7 +2,7 @@
 # monitors directory for .lease file creation and calls script to handle when this happens.
 
 while true; do
-    inotifywait -m -r --include '.*\.(?:lease)\..*' -e modify,create /var/lib/dnsmasq/leases | while read file; do
-        ./wavelet_network_device_sense.sh "$file" &
-    done
+	inotifywait -m --include '.*\.lease' -e modify,create /var/tmp | while read file; do
+		/usr/local/bin/wavelet_network_device.sh "$file" &
+	done
 done
