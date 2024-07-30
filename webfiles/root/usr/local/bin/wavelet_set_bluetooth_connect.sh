778cd6352a8cd47c05ee0d30a120ad9ef1bfc18b
armelvil <103518994+armelvil@users.noreply.github.com>
2024-07-30 17:55:13 -0400

    Update wavelet_set_bluetooth_connect.sh
    Now functional, could probably use improvement

--- webfiles/root/usr/local/bin/wavelet_set_bluetooth_connect.sh
+++ webfiles/root/usr/local/bin/wavelet_set_bluetooth_connect.sh
@@ -6,37 +6,37 @@
 ETCDURI=http://192.168.1.32:2379/v2/keys
 ETCDENDPOINT=192.168.1.32:2379
 read_etcd(){
-		ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get $(hostname)/${KEYNAME} --print-value-only)
-		echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
+	ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get $(hostname)/${KEYNAME} --print-value-only)
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
 }
 
 read_etcd_prefix(){
-		ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix $(hostname)/${KEYNAME} --print-value-only)
-		echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
+	ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix $(hostname)/${KEYNAME} --print-value-only)
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
 }
 
 read_etcd_global(){
-		ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get ${KEYNAME} --print-value-only)
-		echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for Global value"
+	ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get ${KEYNAME} --print-value-only)
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for Global value"
 }
 
 write_etcd(){
-		ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
-		echo -e "${KEYNAME} set to ${KEYVALUE} for $(hostname)"
+	ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
+	echo -e "${KEYNAME} set to ${KEYVALUE} for $(hostname)"
 }
 
 write_etcd_global(){
-		ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "${KEYNAME}" -- "${KEYVALUE}"
-		echo -e "${KEYNAME} set to ${KEYVALUE} for Global value"
+	ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "${KEYNAME}" -- "${KEYVALUE}"
+	echo -e "${KEYNAME} set to ${KEYVALUE} for Global value"
 }
 
 write_etcd_clientip(){
-		# Variable changed to IPVALUE because the module was picking up incorrect variables and applying them to /decoderip !
-		ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put decoderip/$(hostname) "${IPVALUE}"
-		echo -e "decoderip/$(hostname) set to ${IPVALUE} for Global value"
+	# Variable changed to IPVALUE because the module was picking up incorrect variables and applying them to /decoderip !
+	ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put decoderip/$(hostname) "${IPVALUE}"
+	echo -e "decoderip/$(hostname) set to ${IPVALUE} for Global value"
 }
 read_etcd_clients_ip() {
-		ETCDCTL_API=3 return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix decoderip/ --print-value-only)
+	ETCDCTL_API=3 return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix decoderip/ --print-value-only)
 }
 
 main(){
@@ -45,8 +45,17 @@ main(){
 	KEYVALUE="0"
 	write_etcd_global
 
+	# check to see if audio is even enabled, if not, we exit 0
+	KEYNAME="/audio_interface/bluetooth_connect_active"
+	read_etcd_global
+	if [[ "${printvalue}" -eq "0" ]]; then
+	echo -e "\nAudio bit is not enabled, disabling bluetooth and exiting\n"
+	echo -e 'power off\n' | bluetoothctl
+	exit 0
+	fi
+
 	# etcdctl - get bluetooth MAC for ExUBT (set in Audio control portion on webUI)
-	KEYNAME="/audio_interface/bluetooth_mac"
+	KEYNAME="/audio_interface_bluetooth_mac"
 	read_etcd_global
 	bluetoothMAC=${printvalue}
 	# if bluetoothMAC=""; then
@@ -64,6 +73,8 @@ main(){
 	echo -e "trust ${bluetoothMAC}\n" | bluetoothctl
 	echo -e "pair ${bluetoothMAC}\n" | bluetoothctl
 	echo -e "connect ${bluetoothMAC}\n" | bluetoothctl
+	# Clean up to stop unauthorized pairing
+	echo -e 'pairable off\n' | bluetoothctl
 
 	# etcdctl set bluetooth connection successful for webUI tracking
 	KEYNAME="/audio_interface/bluetooth_connect_active"
