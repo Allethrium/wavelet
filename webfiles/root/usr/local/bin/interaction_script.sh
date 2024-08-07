32d7d9f6f60f2ba0347f93d5cff101c02a8495f4
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-07 18:06:47 -0400

    Update interaction_script.sh
    changed key format for consistency

--- webfiles/root/usr/local/bin/interaction_script.sh
+++ webfiles/root/usr/local/bin/interaction_script.sh
@@ -5,7 +5,7 @@
 ETCDURI=http://192.168.1.32:2379/v2/keys
 ETCDENDPOINT=192.168.1.32:2379
 read_etcd(){
-        printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get $(hostname)/${KEYNAME})
+        printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get /$(hostname)/${KEYNAME})
         echo -e "Key Name {$KEYNAME} read from etcd for value ${printvalue} for host $(hostname)"
 }
 
@@ -15,7 +15,7 @@ read_etcd_global(){
 }
 
 write_etcd(){
-        etcdctl --endpoints=${ETCDENDPOINT} put "$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
+        etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
         echo -e "${KEYNAME} set to ${KEYVALUE} for $(hostname)"
 }
 
@@ -25,11 +25,11 @@ write_etcd_global(){
 }
 
 write_etcd_clientip(){
-        etcdctl --endpoints=${ETCDENDPOINT} put decoderip/$(hostname) "${KEYVALUE}"
+        etcdctl --endpoints=${ETCDENDPOINT} put /decoderip/$(hostname) "${KEYVALUE}"
         echo -e "$(hostname) set to ${KEYVALUE} for Global value"
 }
 read_etcd_clients_ip() {
-        return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix decoderip/ --print-value-only)
+        return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix /decoderip/ --print-value-only)
 }
 
 
