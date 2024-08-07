573db0417b97d3bec8bc14289d4c977842a37d8a
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-07 18:07:35 -0400

    Update wavelet_client_poll.sh
    changed key format for consistency

--- webfiles/root/usr/local/bin/wavelet_client_poll.sh
+++ webfiles/root/usr/local/bin/wavelet_client_poll.sh
@@ -8,7 +8,7 @@
 ETCDURI=http://192.168.1.32:2379/v2/keys
 ETCDENDPOINT=192.168.1.32:2379
 read_etcd(){
-        printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get $(hostname)/${KEYNAME} --print-value-only)
+        printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get /$(hostname)/${KEYNAME} --print-value-only)
         echo -e "Key Name {$KEYNAME} read from etcd for value ${printvalue} for host $(hostname)"
 }
 
@@ -18,7 +18,7 @@ read_etcd_global(){
 }
 
 write_etcd(){
-        etcdctl --endpoints=${ETCDENDPOINT} put "$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
+        etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
         echo -e "${KEYNAME} set to ${KEYVALUE} for $(hostname)"
 }
 
@@ -28,11 +28,11 @@ write_etcd_global(){
 }
 
 write_etcd_clientip(){
-        etcdctl --endpoints=${ETCDENDPOINT} put decoderip/$(hostname) "${KEYVALUE}"
+        etcdctl --endpoints=${ETCDENDPOINT} put /decoderip/$(hostname) "${KEYVALUE}"
         echo -e "$(hostname) set to ${KEYVALUE} for Global value"
 }
 read_etcd_clients_ip() {
-        return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix decoderip/ --print-value-only)
+        return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix /decoderip/ --print-value-only)
 }
 
 
