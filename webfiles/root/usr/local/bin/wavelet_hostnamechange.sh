7aad743da6ee8b0da7012b022c1b76819507e894
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-12 17:54:46 -0400

    Update wavelet_hostnamechange.sh
    - preliminary module to handle encoder-decoder up/downgrade as well as hostname label changing

--- webfiles/root/usr/local/bin/wavelet_hostnamechange.sh
+++ webfiles/root/usr/local/bin/wavelet_hostnamechange.sh
@@ -23,16 +23,30 @@ UG_HOSTNAME=$(hostname)
 }
 
 getNewHostName(){
+	prefix="dec"
 	# create an oldhostname file for next reboot
 	echo -e $(hostname) > /home/wavelet/oldhostname.txt
 	# etcdctl get the hash of the device from the watcher
 	HOW?
+
 	#etcdctl get the NEW label of the device
 	etcdctl --endpoints=192.168.1.32:2379 get hostHash/label -- printvalue only
 	# parse the label and make sure we have a valid one and can generate a proper fqdn from it
 		# validation stuff here
-	# 
-	hostnamectl hostname ${1}${printvalue}.wavelet.local
+
+
+	# Check the promotion bit for this host
+	etcdctl --endpoints=192.168.1.32:2379 get /$(hostname)/PROMOTE -- printvalue only
+	if [[ "${printvalue}" -eq "1" ]]; then
+		echo -e "Promotion bit is set to one, the decoder has been instructed to become an encoder..\n"
+		etcdctl --endpoints=192.168.1.32:2379 set /$(hostname)/PROMOTE -- "0"
+		encHostname=$(echo $(hostname) | sed 's/dec/enc/g' input.txt)
+		etcdctl --endpoints=192.168.1.32:2379 set /$(hostname)/IS_PROMOTED -- "1"
+		hostnamectl hostname "${encHostname}.wavelet.local"
+	fi
+	echo -e "Promotion bit is not set to 1, proceeding with hostname change.."
+	# Set the hostname
+	hostnamectl hostname "${prefix}${newHostnameValue}.wavelet.local"
 	# reboot system, build_ug/run_ug will pick everything up from here.
 	systemctl reboot
 }
