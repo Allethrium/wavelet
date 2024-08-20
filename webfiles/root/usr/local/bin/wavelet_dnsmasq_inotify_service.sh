16a9c44c623bf0a1c67faf94cdebfecc28d883c8
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-20 17:48:55 -0400

    Update wavelet_dnsmasq_inotify_service.sh
    - attempting to modify to allow detection/redetection of net devices

--- webfiles/root/usr/local/bin/wavelet_dnsmasq_inotify_service.sh
+++ webfiles/root/usr/local/bin/wavelet_dnsmasq_inotify_service.sh
@@ -2,7 +2,8 @@
 # monitors directory for .lease file creation and calls script to handle when this happens.
 
 while true; do
-	inotifywait -m --include '.*\.lease' -e modify,create /var/tmp | while read file; do
+	# We react only to created files, we don't do anything on deletion or even modification.
+	inotifywait -m --include '.*\.lease' -e create /var/tmp | while read file; do
 		/usr/local/bin/wavelet_network_device.sh "$file" &
 	done
 done
