7661db5252823f3bae359d770205c8a49ed77574
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-20 17:48:25 -0400

    Update monitor_encoderflag.sh
    - add non global etcd interaction as this is now done per hostname

--- webfiles/root/usr/local/bin/monitor_encoderflag.sh
+++ webfiles/root/usr/local/bin/monitor_encoderflag.sh
@@ -11,6 +11,11 @@ read_etcd_global(){
 		echo -e "Key Name {$KEYNAME} read from etcd for value ${printvalue} for Global value"
 }
 
+write_etcd(){
+		etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
+		echo -e "${KEYNAME} set to ${KEYVALUE} for $(hostname)"
+}
+
 read_etcd(){
 		printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get /$(hostname)/${KEYNAME} --print-value-only)
 		echo -e "Key Name {$KEYNAME} read from etcd for value ${printvalue} for host $(hostname)"
@@ -42,7 +47,7 @@ UG_HOSTNAME=$(hostname)
 
 main() {
 # main thread, checks encoder restart flag in etcd
-	KEYNAME="encoder_restart"
+	KEYNAME="/encoder_restart"
 	read_etcd
 	if [[ "${printvalue}" -eq 1 ]]; then
 		echo -e "Encoder restart bit is set! continuing..\n"
@@ -68,7 +73,7 @@ main() {
 		echo -e "Resetting encoder restart flag to 0.."
 		KEYNAME=encoder_restart
 		KEYVALUE=0
-		write_etcd_global
+		write_etcd
 	fi
 }
 
