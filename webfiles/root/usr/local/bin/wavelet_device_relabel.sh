b2fc42de982496333943017edb050e1f9eb48629
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-21 17:53:47 -0400

    Update wavelet_device_relabel.sh
    - update functionality, lots of changes
    - checks for fqdn so it's passed back into etcd properly, therefore the UI will correctly reflect the USB host the device is connected to.

--- webfiles/root/usr/local/bin/wavelet_device_relabel.sh
+++ webfiles/root/usr/local/bin/wavelet_device_relabel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Generates an oldHostName file in /var/tmp and reboots the device. 
-# From there build_ug.sh and run_ug.sh will handle everything.
+# Called from the relabel watcher service
+# Generates an oldHostName file in /home/wavelet and restarts run_ug.sh to start detection process
 
 #Etcd Interaction
 ETCDURI=http://192.168.1.32:2379/v2/keys
@@ -40,10 +40,73 @@ read_etcd_clients_ip() {
 }
 
 detect_self(){
-	echo $(hostname) > /var/tmp/oldhostname.txt
-	KEYNAME="/hosthash/$(hostname)/relabel"
-	hostnamectl hostname ${}
-	systemctl reboot
+systemctl --user daemon-reload
+UG_HOSTNAME=$(hostname)
+				echo -e "Hostname is $UG_HOSTNAME \n"
+				case $UG_HOSTNAME in
+				enc*)                                   echo -e "I am an Encoder \n"            ;       check_label
+				;;
+				dec*)                                   echo -e "I am a Decoder \n"             ;       check_label
+				;;
+				svr*)                                   echo -e "I am a Server \n"              ;       exit 0
+				;;
+				*)                                      echo -e "This device is other \n"       ;       check_label
+				;;
+				esac
+}
+
+
+check_label(){
+	# Finds our current hash and gets the new label from Etcd as set from UI
+	KEYNAME="/hostHash/$(hostname)/relabel_active"
+	KEYVALUE="1"
+	write_etcd_global
+	KEYNAME="/$(hostname)/hash"
+	read_etcd_global
+	myHostHash= ${printvalue}
+	echo -e "My hash is ${myHostHash}, attempting to find a new device label..\n"
+	KEYNAME="/hostHash/${myHostHash}/hostLabel"
+	myNewHostLabel = ${printvalue}
+	echo -e "My new host label is ${myNewHostLabel}!\n"
+	if [[ "$(hostname)" == "${myNewHostLabel}" ]]; then
+		echo -e "New label and current hostname are identical, doing nothing..\n"
+		exit 0
+	else
+		echo -e "New label and current hostname are different, proceding to initiate change.."
+		echo $(hostname) > /home/wavelet/oldhostname.txt
+		get_newLabel
+	fi
+}
+
+
+get_newLabel(){
+	# Finds our current hash and gets the new label from Etcd as set from UI
+	KEYNAME="/hostHash/$(hostname)/relabel_active"
+	KEYVALUE="1"
+	write_etcd_global
+	KEYNAME="/$(hostname)/hash"
+	read_etcd_global
+	myHostHash= ${printvalue}
+	echo -e "My hash is ${myHostHash}, attempting to find a new device label..\n"
+	KEYNAME="/hostHash/${myHostHash}/hostLabel"
+	myNewHostLabel = ${printvalue}
+	echo -e "My new host label is ${myNewHostLabel}!\n"
+	# Check for current FQDN in the case someone wrote gibberish in the text box.
+	FQDN=$(nslookup $(hostname) -i | grep $(hostname) | head -n 1)
+	NAME=${FQDN##*:}
+	echo -e "HostName FQDN is ${NAME}"
+	# Compare current FQDN with input FQDN and append if necessary
+	currentfqdnString=$(${NAME}%.*)
+	inputfqdnString=$(${myNewHostLabel}%.*)
+	if [[ "${currentfqdnString} == ${inputfqdnString}" ]]; then
+		echo -e "FQDN strings are correct, proceeding.."
+		appendedHostName=${myNewHostLabel}
+	else
+		echo -e "New label was input without an FQDN, appending automatically..\n"
+		appendedHostName="${myNewHostLabel}.${currentfqnString}"
+		echo -e "Generated FQDN hostname as ${appendedHostName}\n"
+	fi
+	echo "${appendedHostName}" > newHostName.txt
 }
 
 ###
@@ -53,6 +116,7 @@ detect_self(){
 #
 #
 ###
+
 set -x
 exec >/home/wavelet/changehostname.log 2>&1
 detect_self
