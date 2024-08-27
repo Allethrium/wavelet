c0ef2fea4a65a0e3e65440590cc6e9979730835b
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-27 18:25:44 -0400

    Update get_hosts.php
    - clean module up

--- webfiles/root/home/wavelet/http-php/html/get_hosts.php
+++ webfiles/root/home/wavelet/http-php/html/get_hosts.php
@@ -24,7 +24,7 @@ function poll_etcd_hosts($keyPrefix, $keyPrefixPlusOneBit) {
 		$decodedValue = base64_decode($item['value']);
 		$hostName = (str_replace ("/hostLabel/", "", (str_replace("/type", "", $decodedKey))));
 		array_push($newData, [
-			'key'		=>	$decodedKeyShort,
+			'key'		=>	$decodedKey,
 			'type'		=>	$decodedValue,
 			'hostName'	=> 	(str_replace ("/hostLabel/", "", (str_replace("/type", "", $decodedKey)))),
 			'hostHash'	=>	(str_replace ("\"", "", curl_etcd($hostName))),
@@ -32,6 +32,30 @@ function poll_etcd_hosts($keyPrefix, $keyPrefixPlusOneBit) {
 	}
 	$output = json_encode($newData);
 	echo $output;
+}
+
+function curl_etcd($hostName) {
+		$b64KeyTarget = base64_encode("/$hostName/Hash");
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$b64KeyTarget\"}");
+		$headers = array();
+		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$hashresult = curl_exec($ch);
+		if (curl_errno($ch)) {
+				echo 'Error:' . curl_error($ch);
+		}
+		curl_close($ch);
+		$hashDataArray = json_decode($hashresult, true); // this decodes the JSON string as an associative array
+		foreach ($hashDataArray['kvs'] as $x => $item) {
+				$decodedHashKey = base64_decode($item['key']);
+				$decodedHashValue = base64_decode($item['value']);
+				$hostHashDecoded = json_encode($decodedHashValue);
+		return $hostHashDecoded;
+		}
 }
 
 // modify these strings if you want to get a different key prefix out of etcd.  For Wavelet, there's no reason to change them.
