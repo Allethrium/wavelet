f0cd367a50d4d0a82fc3bc3f0c94c3225f62513a
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-27 19:18:32 -0400

    Create wavelet_promote.sh
    - handles encoder/decoder state change

---
+++ webfiles/root/usr/local/bin/wavelet_promote.sh
@@ -0,0 +1,67 @@
+#!/bin/bash 
+#
+# This script resets the appropriate flag back to 0 and then calls hostname change.
+
+detect_self(){
+systemctl --user daemon-reload
+UG_HOSTNAME=$(hostname)
+				echo -e "Hostname is $UG_HOSTNAME \n"
+				case $UG_HOSTNAME in
+				enc*)                                   echo -e "I am an Encoder \n"           				;		event_encoder
+				;;
+				dec*)                                   echo -e "I am a Decoder \n"                 	    ;		event_decoder
+				;;
+				svr*)                                   echo -e "I am a Server, ending process \n"			;		exit 0
+				;;
+				*)                                      echo -e "This device is other, ending process\n"	;		exit 0
+				;;
+				esac
+}
+
+#Etcd Interaction
+ETCDURI=http://192.168.1.32:2379/v2/keys
+ETCDENDPOINT=192.168.1.32:2379
+ETCDCTL_API=3
+
+read_etcd_global(){
+	ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get ${KEYNAME} --print-value-only)
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for Global value"
+}
+
+event_decoder(){
+	echo -e "\nPromotion from Decoder to Encoder flag change detected, resetting flag and calling host name change module..\n"
+	systemctl --user disable UltraGrid.AppImage.service --now
+	etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/PROMOTE" -- "0"
+	etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/RELABEL" -- "1"
+	systemctl --user enable wavelet_promote.service --now
+	/usr/local/bin/wavelet_device_relabel.sh "DEC"
+	exit 0
+}
+
+event_encoder(){
+	echo -e "\nPromotion from Encoder to Decoder flag change detected, resetting flag and calling host name change module..\n"
+	systemctl --user disable UltraGrid.AppImage.service --now
+	etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/PROMOTE" -- "0"
+	etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/RELABEL" -- "1"
+	systemctl --user enable wavelet_promote.service --now
+	/usr/local/bin/wavelet_device_relabel.sh "ENC"
+	exit 0
+}
+
+###
+#
+# Main 
+#
+###
+
+exec >/home/wavelet/promote.log 2>&1
+
+KEYNAME=/$(hostname)/PROMOTE
+read_etcd_global
+if [[ "${printvalue}" == 1 ]]; then
+	echo -e "\nPROMOTE key is set to 1, continuing with task.. \n"
+	detect_self
+else
+	echo -e "\nPROMOTE key is set to 0, doing nothing.. \n"
+	exit 0
+fi
