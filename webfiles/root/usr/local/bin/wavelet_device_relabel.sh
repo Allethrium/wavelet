f068d0f607936177d694476ce65d62d9567b333c
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-27 19:18:22 -0400

    Update wavelet_device_relabel.sh
    - working but needs the host name change code placed in this module rather than build/run_ug

--- webfiles/root/usr/local/bin/wavelet_device_relabel.sh
+++ webfiles/root/usr/local/bin/wavelet_device_relabel.sh
@@ -6,53 +6,53 @@
 ETCDURI=http://192.168.1.32:2379/v2/keys
 ETCDENDPOINT=192.168.1.32:2379
 read_etcd(){
-		ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get /$(hostname)/${KEYNAME} --print-value-only)
-		echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
+	ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get /$(hostname)/${KEYNAME} --print-value-only)
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
 }
 
 read_etcd_prefix(){
-		ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix /$(hostname)/${KEYNAME} --print-value-only)
-		echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
+	ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix /$(hostname)/${KEYNAME} --print-value-only)
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)"
 }
 
 read_etcd_global(){
-		ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get ${KEYNAME} --print-value-only)
-		echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for Global value"
+	ETCDCTL_API=3 printvalue=$(etcdctl --endpoints=${ETCDENDPOINT} get ${KEYNAME} --print-value-only)
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for Global value"
 }
 
 write_etcd(){
-		ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
-		echo -e "${KEYNAME} set to ${KEYVALUE} for $(hostname)"
+	ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "/$(hostname)/${KEYNAME}" -- "${KEYVALUE}"
+	echo -e "${KEYNAME} set to ${KEYVALUE} for $(hostname)"
 }
 
 write_etcd_global(){
-		ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "${KEYNAME}" -- "${KEYVALUE}"
-		echo -e "${KEYNAME} set to ${KEYVALUE} for Global value"
+	ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put "${KEYNAME}" -- "${KEYVALUE}"
+	echo -e "${KEYNAME} set to ${KEYVALUE} for Global value"
 }
 
 write_etcd_clientip(){
-		# Variable changed to IPVALUE because the module was picking up incorrect variables and applying them to /decoderip !
-		ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put /decoderip/$(hostname) "${IPVALUE}"
-		echo -e "decoderip/$(hostname) set to ${IPVALUE} for Global value"
+	# Variable changed to IPVALUE because the module was picking up incorrect variables and applying them to /decoderip !
+	ETCDCTL_API=3 etcdctl --endpoints=${ETCDENDPOINT} put /decoderip/$(hostname) "${IPVALUE}"
+	echo -e "decoderip/$(hostname) set to ${IPVALUE} for Global value"
 }
 read_etcd_clients_ip() {
-		ETCDCTL_API=3 return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix /decoderip/ --print-value-only)
+	ETCDCTL_API=3 return_etcd_clients_ip=$(etcdctl --endpoints=${ETCDENDPOINT} get --prefix /decoderip/ --print-value-only)
 }
 
 detect_self(){
 systemctl --user daemon-reload
 UG_HOSTNAME=$(hostname)
-				echo -e "Hostname is $UG_HOSTNAME \n"
-				case $UG_HOSTNAME in
-				enc*)                                   echo -e "I am an Encoder \n"            ;       check_label
-				;;
-				dec*)                                   echo -e "I am a Decoder \n"             ;       check_label
-				;;
-				svr*)                                   echo -e "I am a Server \n"              ;       exit 0
-				;;
-				*)                                      echo -e "This device is other \n"       ;       check_label
-				;;
-				esac
+	echo -e "Hostname is $UG_HOSTNAME \n"
+	case $UG_HOSTNAME in
+		enc*)                                   echo -e "I am an Encoder \n"            ;       check_label
+		;;
+		dec*)                                   echo -e "I am a Decoder \n"             ;       check_label
+		;;
+		svr*)                                   echo -e "I am a Server \n"              ;       exit 0
+		;;
+		*)                                      echo -e "This device is other \n"       ;       check_label
+		;;
+		esac
 }
 
 
@@ -65,7 +65,7 @@ check_label(){
 	read_etcd_global
 	myHostHash="${printvalue}"
 	echo -e "My hash is ${myHostHash}, attempting to find a my Current device label..\n"
-	KEYNAME="/hostHash/${myHostHash}/hostLabel"
+	KEYNAME="/hostHash/${myHostHash}/newHostLabel"
 	myNewHostLabel="${printvalue}"
 	echo -e "My *New* host label is ${myNewHostLabel}!\n"
 	if [[ "$(hostname)" == "${myNewHostLabel}" ]]; then
@@ -81,7 +81,6 @@ check_label(){
 	fi
 }
 
-
 set_newLabel(){
 	# Finds our current hash and gets the new label from Etcd as set from UI
 	KEYNAME="/$(hostname)/Hash"
@@ -94,7 +93,7 @@ set_newLabel(){
 	echo -e "My new host label is ${myNewHostLabel}!\n"
 	# Check for current FQDN in the case someone wrote gibberish in the text box.
 	FQDN=$(nslookup $(hostname) -i | grep $(hostname) | head -n 1)
-	NAME=${FQDN##*:}
+	NAME=$(echo "${FQDN##*:}" | xargs)
 	echo -e "HostName FQDN is ${NAME}"
 	# Compare current FQDN with input FQDN and append if necessary
 	currentfqdnString=$(echo "${NAME}%.*")
@@ -111,29 +110,77 @@ set_newLabel(){
 	KEYNAME="/hostHash/${myHostHash}/newHostLabel"
 	# remove the newHostLabel value
 	etcdctl --endpoints=${ETCDENDPOINT} del ${KEYNAME}
-	# At this point we issue a reboot call and run_ug/build_ug handle the rest upon reboot
+	# Generate the necessary files, then reboot and let run/build_ug handle everything from there.
+	systemctl reboot
+}
+
+event_prefix_set(){
+	# Finds our current hash and gets the current label from Etcd as set from UI
+	KEYNAME="/$(hostname)/Hash"
+	read_etcd_global
+	myHostHash="${printvalue}"
+	KEYNAME="/hostHash/${myHostHash}"
+	read_etcd_global
+	myHostLabel="${printvalue}"
+	echo -e "My host label is ${myHostLabel}!\n"
+	FQDN=$(nslookup $(hostname) -i | grep $(hostname) | head -n 1)
+	NAME=$(echo "${FQDN##*:}" | xargs)
+	echo -e "HostName FQDN is ${NAME}"
+	# Compare current FQDN with input FQDN and append if necessary
+	currentfqdnString=${NAME}
+	inputfqdnString=${myHostLabel}
+	if [[ "${currentfqdnString}" == "${inputfqdnString}" ]]; then
+		echo -e "FQDN strings are correct, proceeding.."
+		appendedHostName=${inputfqdnString}
+	else
+		echo -e "New label was input without an FQDN, appending automatically..\n"
+		appendedHostName="${myNewHostLabel}.${currentfqnString}"
+		echo -e "Generated FQDN hostname as ${appendedHostName}\n"
+	fi
+	# Replace dec with enc or enc with dec as appropriate..
+	if [[ "${arg}" == "dec" ]]; then
+		appendedHostName=$(echo -e "${appendedHostName}" | sed "s|${arg}|enc|g")
+	else
+		appendedHostName=$(echo -e "${appendedHostName}" | sed "s|${arg}|dec|g")
+	fi
+	# Generate the necessary files, then reboot and let run/build_ug handle everything from there.
+	echo -e "Switched host identifier from ${arg}, generating ref file as ${appendedHostName}, and instructing host to reboot..\n"
+	KEYNAME="/hostHash/${myHostHash}/newHostLabel"
+	KEYVALUE="${appendedHostName}"
+	write_etcd_global
+	echo $(hostname) > /home/wavelet/oldhostname.txt
+	echo -e "${appendedHostName}" > newHostName.txt
 	systemctl reboot
 }
 
 ###
 #
-#
-# Execute script
-#
+# Main
 #
 ###
 
+# Parse input options (I.E if called by promote service)
+for arg in "$@"; do
+	case  ${arg^^} in
+		DEC*)   echo -e "Called with decoder argument, promoting to Encoder\n" 			 	;	event_prefix_set "${arg}"; exit 0
+		;;
+		ENC*)   echo -e "Called with encoder argument, promoting to Decoder\n"  			;	event_prefix_set "${arg}"; exit 0
+		;;
+		*)  	echo -e "Called with invalid argument, not calling prefix function..\n"		;
+	esac
+done
 
-name=/home/wavelet/changehostname.log
-if [[ -e $name.ext || -L $name.ext ]] ; then
-    i=0
-    while [[ -e $name-$i.ext || -L $name-$i.ext ]] ; do
-        let i++
-    done
-    name=$name-$i
+# Check for pre-existing log file
+# This is necessary because of system restarts, the log will get overwritten, and we need to see what it's doing across reboots.
+logName=/home/wavelet/changehostname.log
+if [[ -e $logName || -L $logName ]] ; then
+	i=0
+	while [[ -e $logName-$i || -L $logName-$i ]] ; do
+		let i++
+	done
+	logName=$logName-$i
 fi
-exec > "${name}".ext 2>&1
-
+exec > "${logName}" 2>&1
 
 KEYNAME="/$(hostname)/RELABEL"
 read_etcd_global
