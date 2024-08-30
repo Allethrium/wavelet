40176e795bb19c6f2cabce090ad68aa2624c8a99
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-30 16:53:41 -0400

    Update set_remove_input.php
    - now handles both network and local devices, and removes all keys properly

--- webfiles/root/home/wavelet/http-php/html/set_remove_input.php
+++ webfiles/root/home/wavelet/http-php/html/set_remove_input.php
@@ -3,121 +3,68 @@ header('Content-type: application/json');
 $key = $_POST["key"];
 $value = $_POST["value"];
 
-function delete_input_labels($key) {
-	$prefixstring = "/interface/$key";
+function del_etcd($input) {
+	$prefixstring = "$input";
 	$prefixstringplusOne = "$prefixstring" . "0";
 	$keyPrefix=base64_encode($prefixstring);
 	$keyPrefixPlusOneBit=base64_encode($prefixstringplusOne);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/deleterange');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$keyPrefix\", \"range_end\": \"$keyPrefixPlusOneBit\"}");
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/deleterange');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$keyPrefix\", \"range_end\": \"$keyPrefixPlusOneBit\"}");
 	$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+	$headers[] = 'Content-Type: application/x-www-form-urlencoded';
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	error_log("Calling delete_input_labels() function for key prefix \"$prefixString\""); // Log the function call
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			echo 'Error:' . curl_error($ch);
+	error_log("Calling delete_input_labels() function for key prefix \"$prefixstring\"");
+	$result = curl_exec($ch);
+	if (curl_errno($ch)) {
+		echo 'Error:' . curl_error($ch);
 	}
+	echo "\n$input range removed";
 }
 
-function delete_network_input_labels($key) {
-	$prefixstring = "$key";
-	$prefixstringplusOne = "$prefixstring" . "0";
-	$keyPrefix=base64_encode($prefixstring);
-	$keyPrefixPlusOneBit=base64_encode($prefixstringplusOne);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/deleterange');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$keyPrefix\", \"range_end\": \"$keyPrefixPlusOneBit\"}");
+function get_device_ip($input){
+	echo "looking for $input";
+	$networkIPHashValue = base64_encode("$input");
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$networkIPHashValue\"}");
 	$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+	$headers[] = 'Content-Type: application/x-www-form-urlencoded';
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	error_log("Calling delete_input_labels() function for key prefix \"$prefixString\""); // Log the function call
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			echo 'Error:' . curl_error($ch);
+	$IPresult = curl_exec($ch);
+	if (curl_errno($ch)) {
+		echo 'Error:' . curl_error($ch);
 	}
+	curl_close($ch);
+	$dataIPArray = json_decode($IPresult, true);
+	foreach ($dataIPArray['kvs'] as $x => $item) {
+		$decodedValue = base64_decode($item['value']);
+	}
+	return $decodedValue;
+	echo "\nFound: $decodedValue";
 }
 
-function delete_short_hash($value) {
-	$prefixstring = "/short_hash/$value";
-	$keyPrefix=base64_encode($prefixstring);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/deleterange');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$keyPrefix\"}");
-	$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			echo 'Error:' . curl_error($ch);
-	}
-	echo "$key removed from /short_hash/ ..";
-}
-
-function delete_network_hashes($value) {
-	$prefixstring = "$value";
-	$keyPrefix=base64_encode($prefixstring);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/deleterange');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$keyPrefix\"}");
-	$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			echo 'Error:' . curl_error($ch);
-	}
-	echo "$key removed from $value..";
-}
-
-function delete_hash($value) {
-	$prefixstring = "/hash/$value";
-	$keyPrefix=base64_encode($prefixstring);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/deleterange');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$keyPrefix\"}");
-	$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			echo 'Error:' . curl_error($ch);
-	}
-}
-
-
-// Key in this context is the label of the device to be removed, value would be the hash of the device to be removed.
-
-if (str_contains ($key, '/network_interface/short/')) {
-                echo "This is a network device, calling appropriate function for network device..";
-                delete_network_input_labels($key);
-                // we need to get the network_ip and the network_longhash data from etcd to complete this process..somehow
-                // get network_ip value store in $ipValue variable and delete the entire entry
-                // get network_longhash value by searching etcd for the $ipValue and delete the entire entry
-                delete_network_hashes("network_ip/$value");
-                delete_network_hashes("/network_longhash/$value");
-                delete_network_hashes("/network_shorthash/$value");
+echo "PHP set_remove_input Received removal request for Key:\n$key\nAnd value:\n$value\n";
+if (str_contains ($value, '/network_ip/')) {
+                echo "\nThis is a network device, calling appropriate function for network device..\n";
+                $modHash=(str_replace("/network_ip/", "", $value));
+                # Extract network IP and use it to delete
+                $ipAddr=get_device_ip($value);
+                echo "\nIP is: $ipAddr";
+                del_etcd("/network_uv_stream_command/$ipAddr");
+                del_etcd("$key");
+                del_etcd("/network_ip/$modHash");
+                del_etcd("/network_longhash/$modHash");
+                del_etcd("/network_shorthash/$modHash");
         } else {
-                echo "This is a local device, calling appropriate function for local device..";
-                delete_input_labels($key);
-                delete_hash($value);
-                delete_short_hash($value);
-        }
-
-
-
-// Key is label, value is the hash
-
-echo "Requested Label and tracking data have been removed from Wavelet - subsequent redetections will clear old data and repopulate as a fresh device, if it entered some kind of failure mode.  If this is a network device please try to ensure it's reset to factory defaults so Wavelet will be able to reconfigure it.";
+                echo "\nThis is a local device, calling appropriate function for local device..\n";
+                del_etcd($key);
+                del_etcd("/hash/$value");
+                del_etcd("/short_hash/$value");
+                del_etcd("/long_interface/$value");
+}
 ?>
