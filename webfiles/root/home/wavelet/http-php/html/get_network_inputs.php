05038d9314b8bf8224a04722714d54a6557db217
armelvil <103518994+armelvil@users.noreply.github.com>
2024-08-30 16:49:22 -0400

    Update get_network_inputs.php
    - cleaned and now provides network device IP

--- webfiles/root/home/wavelet/http-php/html/get_network_inputs.php
+++ webfiles/root/home/wavelet/http-php/html/get_network_inputs.php
@@ -2,37 +2,58 @@
 header('Content-type: application/json');
 // this script curls etcd for available NETWORKED input devices.  
 function poll_etcd_inputs($keyPrefix, $keyPrefixPlusOneBit) {
-		$ch = curl_init();
-		// CURL example:  curl -L http://192.168.1.32:2379/v3/kv/range -X POST -d '{"key": "L2ludGVyZmFjZQ==", "range_end": "L2ludGVyZmFjZTA="}'
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$keyPrefix\", \"range_end\": \"$keyPrefixPlusOneBit\"}");
-		$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			echo 'Error:' . curl_error($ch);
-		}
-		curl_close($ch);
-		$dataArray = json_decode($result, true); // this decodes the JSON string as an associative array
-		foreach ($dataArray['kvs'] as $x => $item) {
-		// This forms the "pretty" label, the value that gets changed when the relabel key is picked.
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$keyPrefix\", \"range_end\": \"$keyPrefixPlusOneBit\"}");
+	$headers = array();
+	$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	$result = curl_exec($ch);
+	if (curl_errno($ch)) {
+		echo 'Error:' . curl_error($ch);
+	}
+	curl_close($ch);
+	$dataArray = json_decode($result, true);
+	foreach ($dataArray['kvs'] as $x => $item) {
 		$decodedKeyShort = (str_replace("-video-index0", "", (str_replace("/network_interface/short/", "", (base64_decode($item['key']))))));
-		// This is the hash value of the device, and is used to track the device state if it is unplugged/plugged back in to the same port
 		$decodedKey = base64_decode($item['key']);
-		// This is the "long" device name which is used as a reverse lookup w/ the device hash
 		$decodedValue = base64_decode($item['value']);
-
-				$newData[] = [
-						'value' => $decodedValue,
-						'key' => $decodedKey,
-						'keyFull' => $decodedKey
-				];
+		$decodedShortHash = (str_replace("/network_interface/", "", base64_decode($item['value'])));
+		$ipValue=get_device_ip($decodedShortHash);
+			$newData[] = [
+				'value' => $decodedValue,
+				'key' => $decodedKeyShort,
+				'keyFull' => $decodedKey,
+				'IP' => $ipValue
+			];
 		}
-		$output = json_encode($newData);
-		echo $output;
+	$output = json_encode($newData);
+	echo $output;
+}
+
+
+function get_device_ip($decodedShortValue){
+	$networkIPHashValue = base64_encode("/network_ip/$decodedShortValue");
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$networkIPHashValue\"}");
+	$headers = array();
+	$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	$IPresult = curl_exec($ch);
+	if (curl_errno($ch)) {
+		echo 'Error:' . curl_error($ch);
+	}
+	curl_close($ch);
+	$dataIPArray = json_decode($IPresult, true);
+	foreach ($dataIPArray['kvs'] as $x => $item) {
+		$decodedValue = base64_decode($item['value']);
+		return $decodedValue;
+	}
 }
 
 // modify these strings if you want to get a different key prefix out of etcd.  For Wavelet, there's no reason to change them.
