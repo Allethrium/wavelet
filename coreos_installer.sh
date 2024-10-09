4326c7606cb4bb95b3cb0e0755f09e969d9bcca8
armelvil <103518994+armelvil@users.noreply.github.com>
2024-10-09 18:07:47 -0400

    Update coreos_installer.sh
    - made some changes to increase flexibility and allow for image generation after the server has provisioned

--- coreos_installer.sh
+++ coreos_installer.sh
@@ -1,16 +1,27 @@
-# Be sure to specify the correct HDD - you can test this by booting a live linux distro, going to terminal
-# try using lsblk at the terminal to list available drives and partitions.
-#
-#/usr/libexec/coreos-installer -d $hddSDx$ -b file:///$usbmountpoint$/fedora-coreos.raw.xz -i file:///$usbmountpoint$/ignition.json
+#!/bin/bash
 
-echo "Removing old customized ISO files.."
-rm -rf wavelet_server.iso
-rm -rf wavelet_decoder.iso
+for i in "$@"
+	do
+		case $i in
+			"D")	echo -e "\nProvisioning mode enabled, running from server and provisioning Decoder ISO only..\n."
+			;;
+			*)		echo -e "\nInitial Setup mode running, configuring Server + Decoder ISO Files..\n"	;	serverMode="1"
+			;;
+		esac
+done
 
-echo -e "\n  Note: Put the drive controller for target device in AHCI mode from BIOS setup!  \n"
+echo "Removing old customized ISO files if they exist.."
+if [[ "${serverMode}" = "1" ]]; then
+	rm -rf ${HOME}/Downloads/wavelet_server.iso
+	rm -rf ${HOME}/Downloads/wavelet_decoder.iso
+fi
+
+echo -e "\n	Note: Put the drive controller for target devices in AHCI mode from BIOS setup! RAID or other modes have been observed to interfere with the process.\n"
 #read -p "Please input the FULL path of your destination device I.E /dev/nvme0n1.  Could also be /dev/vda if VM:" DESTINATION_DEVICE
-DESTINATION_DEVICE="/dev/nvme0n1"
-echo -e "Destination device is set to ${DESTINATION_DEVICE}, please verify!"
+#DESTINATION_DEVICE="/dev/nvme0n1"
+# Symlink as per: https://docs.fedoraproject.org/en-US/fedora-coreos/storage/
+DESTINATION_DEVICE="/dev/disk/by-id/coreos-boot-disk"
+#echo -e "Destination device is set to ${DESTINATION_DEVICE}, please verify!"
 #read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit
 
 FILEPREFIX="fedora-coreos-"
@@ -18,17 +29,41 @@ if ls ./$FILEPREFIX* > /dev/null 2>&1; then
 	echo "CoreOS image already exists."
 	:
 	else
-	echo "No ISO found, downloading CoreOS ISO from internet.."
+	echo "No ISO found, downloading CoreOS ISO from internet, please be patient while this task executes..\n"
 	coreos-installer download -s stable -a x86_64 -p metal -f iso
 fi
 
+# Find Image file and generate ignition files utilizing Butane
 IMAGEFILE=$(ls -t *.iso | head -n1)
-echo "Generating Ignition files with Butane.."
-butane --pretty --strict ./server_custom.yml --output server.ign
-#butane --pretty --strict ./encoder_custom.yml --output encoder.ign
-butane --pretty --strict ./decoder_custom.yml --output decoder.ign
-echo "Customizing ISO files with Ignition"
-coreos-installer iso customize --dest-device ${DESTINATION_DEVICE} --dest-ignition decoder.ign -o $HOME/Downloads/wavelet_decoder.iso ${IMAGEFILE}
-#coreos-installer iso customize --dest-device ${DESTINATION_DEVICE} --dest-ignition encoder.ign -o $HOME/Downloads/wavelet_encoder.iso ${IMAGEFILE}
-coreos-installer iso customize --dest-device ${DESTINATION_DEVICE} --dest-ignition server.ign -o $HOME/Downloads/wavelet_server.iso ${IMAGEFILE}
-echo -e "Images generated, server will subsequently bootstrap everything. \n ensure it's setup before attempting to install another device, or their installation will fail. \n"
+	echo "Generating Ignition files with Butane..\n"
+	if [[ "${serverMode}" = "1" ]]; then
+		# Generate butane setting files-dir to current path
+		# This is so that we can utilize the local files argument in server_custom.yml to inject decoder.ign file
+		butane --pretty --strict --files-dir ./ ./server_custom.yml --output server.ign
+	fi
+	# Generate decoder.ign regardless of server provisioning argument
+	butane --pretty --strict ./decoder_custom.yml --output decoder.ign
+
+echo "Customizing ISO files with Ignition\n"
+	if [[ "${serverMode}" = "1" ]]; then
+		echo -e "\nProvisioning server and decoder, .ISO files output to ${HOME}/Downloads..\n"
+		# Generate Server ISO
+		coreos-installer iso customize \
+		--dest-device ${DESTINATION_DEVICE} \
+		--dest-ignition server.ign \
+		-o $HOME/Downloads/wavelet_server.iso ${IMAGEFILE}
+		# Generate Decoder ISO
+		coreos-installer iso customize \
+		--dest-device ${DESTINATION_DEVICE} \
+		--dest-ignition decoder.ign \
+		-o $HOME/Downloads/wavelet_decoder.iso ${IMAGEFILE}
+	else
+		echo -e "\nProvisioning Decoder, .ISO files output to /var/wavelet/http/ignition/..\n"
+		# Generate only Decoder ISO
+		coreos-installer iso customize \
+		--dest-device ${DESTINATION_DEVICE} \
+		--dest-ignition /var/home/wavelet/http/ignition/decoder.ign \
+		-o /var/home/wavelet/http/ignition/wavelet_decoder.iso ${IMAGEFILE}
+	fi
+
+echo -e "Image(s) generated,\n If this is initial setup please burn wavelet_server.iso to a USB stick and boot to continue setup..\n"
