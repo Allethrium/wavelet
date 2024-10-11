c9fcef210f828c70ab84ae86b61b913fd0e7bed7
armelvil <103518994+armelvil@users.noreply.github.com>
2024-10-11 12:41:30 -0400

    Update coreos_installer.sh
    - clean up informational messages and properly handle imaging mode

--- coreos_installer.sh
+++ coreos_installer.sh
@@ -16,7 +16,7 @@ if [[ "${serverMode}" = "1" ]]; then
 	rm -rf ${HOME}/Downloads/wavelet_decoder.iso
 fi
 
-echo -e "\n	Note: Put the drive controller for target devices in AHCI mode from BIOS setup! RAID or other modes have been observed to interfere with the process.\n"
+echo -e "\n	**	Note: Put the drive controller for target devices in AHCI mode from BIOS setup!\n	**	RAID or other modes have been observed to interfere with the process.\n"
 #read -p "Please input the FULL path of your destination device I.E /dev/nvme0n1.  Could also be /dev/vda if VM:" DESTINATION_DEVICE
 #DESTINATION_DEVICE="/dev/nvme0n1"
 # Symlink as per: https://docs.fedoraproject.org/en-US/fedora-coreos/storage/
@@ -26,33 +26,37 @@ DESTINATION_DEVICE="/dev/disk/by-id/coreos-boot-disk"
 
 FILEPREFIX="fedora-coreos-"
 if ls ./$FILEPREFIX* > /dev/null 2>&1; then
-	echo "CoreOS image already exists."
+	echo -e "\nCoreOS image already exists.\n"
 	:
 	else
-	echo "No ISO found, downloading CoreOS ISO from internet, please be patient while this task executes..\n"
+	echo -e "\nNo ISO found, downloading CoreOS ISO from internet, please be patient while this task executes..\n"
 	coreos-installer download -s stable -a x86_64 -p metal -f iso
 fi
 
 # Find Image file and generate ignition files utilizing Butane
 IMAGEFILE=$(ls -t *.iso | head -n1)
-	echo "Generating Ignition files with Butane..\n"
+	echo -e "Generating Ignition files with Butane..\n"
+	# Generate decoder.ign regardless of server provisioning argument
+	echo -e "Generating decoder.ign..\n"
+	butane --pretty --strict --files-dir ./ ./decoder_custom.yml --output decoder.ign
 	if [[ "${serverMode}" = "1" ]]; then
 		# Generate butane setting files-dir to current path
 		# This is so that we can utilize the local files argument in server_custom.yml to inject decoder.ign file
+		echo -e "Generating server.ign..\n"
 		butane --pretty --strict --files-dir ./ ./server_custom.yml --output server.ign
 	fi
-	# Generate decoder.ign regardless of server provisioning argument
-	butane --pretty --strict ./decoder_custom.yml --output decoder.ign
 
 echo "Customizing ISO files with Ignition\n"
 	if [[ "${serverMode}" = "1" ]]; then
-		echo -e "\nProvisioning server and decoder, .ISO files output to ${HOME}/Downloads..\n"
+		echo -e "\nISO files output to ${HOME}/Downloads..\n"
 		# Generate Server ISO
+		echo -e "Provision Server ISO..\n"
 		coreos-installer iso customize \
 		--dest-device ${DESTINATION_DEVICE} \
 		--dest-ignition server.ign \
 		-o $HOME/Downloads/wavelet_server.iso ${IMAGEFILE}
 		# Generate Decoder ISO
+		echo -e "Provision Decoder ISO..\n"
 		coreos-installer iso customize \
 		--dest-device ${DESTINATION_DEVICE} \
 		--dest-ignition decoder.ign \
