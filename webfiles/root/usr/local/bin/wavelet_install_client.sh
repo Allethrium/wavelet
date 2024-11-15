499cc008117c494dd7af127e33b5452269f6c8b5
armelvil <103518994+armelvil@users.noreply.github.com>
2024-11-15 10:48:40 -0500

    Update wavelet_install_client.sh
    WIP WIP WIP
    - this will eventually add the security layer enrollment to clients once I've finished the server side.

--- webfiles/root/usr/local/bin/wavelet_install_client.sh
+++ webfiles/root/usr/local/bin/wavelet_install_client.sh
@@ -23,6 +23,30 @@ extract_usrlocalbin(){
 	echo -e "Wavelet application modules setup successfully..\n"
 }
 
+install_security_layer(){
+	# This function checks for the presence of the security layer flag, and if it exists we run domain enrollment
+	if [[ -f /var/prod.security.enabled ]]; then
+		echo -e "Security layer is enabled.. enrolling to domain.\n\nThis will fail if a DC is not available on the wavelet network!\n"
+		# This password should be prepopulated from the DC as part of the initial imaging process.
+		# Umm.. how.  The Server would know a pxe installation request and may have a mac address and IP to play with from dnsmasq.. 
+		# that's the only ID info we will get from that part...
+
+		# Perhaps unattended might work this way..
+		password=$(cat /var/secrets/domainEnrollment.password)
+		user=$(cat /var/secrets/domainEnrollment.userAccount)
+		# These files will be empty if security is not provisioned on the server.
+		waveletDomain=$(cat /var/secrets/wavelet.domain)
+		waveletServer=$(cat /var/secrets/wavelet.server)
+		rm -rf /var/secrets/domainEnrollment.password
+		rm -rf /var/secrets/domainEnrollment.userAccount
+		ipa-client-install --principal "${user}" --password "${password}" --domain ${waveletDomain} --server ${waveletServer} --unattended
+
+		# Reconfigure etcd to utilize certificates
+		echo -e "Reconfiguring etcd client..\n"
+		# Reconfigure WiFi to utilize EAP-TTLS
+		echo -e "Reconfiguring WiFi supplicant..\n"
+}
+
 ####
 #
 # Main
@@ -36,6 +60,7 @@ extract_base
 extract_home
 extract_usrlocalbin
 sleep 2
+install_security_layer
 touch /var/client_install.complete
 systemctl disable wavelet_install_client.service
 connectwifi
