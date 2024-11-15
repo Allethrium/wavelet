81ebed503c10ee39b7c41ddd33dd7baf006cdf75
armelvil <103518994+armelvil@users.noreply.github.com>
2024-11-15 10:50:16 -0500

    Update wavelet_installer_xf.sh
    - further streamline the decoder spinup process
    - add possibility of containerbuilds for other devices
    - add security layer switching

--- webfiles/root/usr/local/bin/wavelet_installer_xf.sh
+++ webfiles/root/usr/local/bin/wavelet_installer_xf.sh
@@ -24,11 +24,31 @@ UG_HOSTNAME=$(hostname)
 }
 
 event_decoder(){
-	rpm_overlay_install_decoder
+	# First we'd need to determine our architecture.
+	arch=$(uname -m)
+	case ${arch} in;
+		"x86_64") determine_ifSurface
+		;;
+		"arm") rpm_ostree_ARM
+		;;
+		"riscV") rpm_ostree_RISCV
+		;;
+		*) echo -e "Architecture obsolete or not supported, exiting..\n"
+	esac
+
+
+	elif [[ -f /var/riscV_support.flag ]];; then
+		# This is more like a stub for completeness' sake,
+		# as I don't see this being useful until a RISC V platform with a real hardware video acceleration core becomes mainstream..
+		echo -e "RISC V support is enabled, building RISC V OCI overlay and downloading and building UG From source..\n"
+		rpm_ostree_RISCV
+	fi
 	exit 0
 }
 
 event_server(){
+	#	Server can only be x86.  I haven't had access to another platform with video hardware support + enough number crunching power to do the task.
+	#	Suggestions which aren't too exotic are welcome!
 	# Generate RPM Container overlay
 	cp /usr/local/bin/wavelet_install_ug_depends.sh	/home/wavelet/containerfiles/
 	cp /usr/local/bin/wavelet_pxe_grubconfig.sh		/home/wavelet/containerfiles/
@@ -36,36 +56,57 @@ event_server(){
 	# generate a hostname file so that dnsmasq's dhcp-script call works properly
 	get_ipValue
 	sed -i "s/SVR_IPADDR/${IPVALUE}/g" /etc/dnsmasq.conf
-	# Generate and enable systemd units which will be enabled
+
+
+	# Generate and enable systemd units
 	# Therefore, they will start on next boot, run, and disable themselves
-	# None of them should require a further reboot
+	# Installing the security layer will require two reboots, one for the domain enrollment and one to move to userland.
 	echo -e "\
-		[Unit]
-		Description=Install Dependencies
-		ConditionPathExists=/var/rpm-ostree-overlay.rpmfusion.pkgs.complete
-        After=multi-user.target
-        [Service]
-        Type=oneshot
-        ExecStart=/usr/bin/bash -c "/usr/local/bin/wavelet_install_ug_depends.sh"
-        ExecStartPost=systemctl disable wavelet_install_depends.service
-        [Install]
-        WantedBy=multi-user.target" > /etc/systemd/system/wavelet_install_depends.service
+[Unit]
+Description=Install Dependencies
+ConditionPathExists=/var/rpm-ostree-overlay.rpmfusion.pkgs.complete
+After=multi-user.target
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/bash -c "/usr/local/bin/wavelet_install_ug_depends.sh"
+ExecStartPost=systemctl disable wavelet_install_depends.service
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/wavelet_install_depends.service
+
 	echo -e "\
-		[Unit]
-		Description=Install PXE support
-		ConditionPathExists=/var/wavelet_depends.complete
-		After=multi-user.target
-        [Service]
-        Type=oneshot
-        ExecStart=/usr/bin/bash -c "/usr/local/bin/wavelet_pxe_grubconfig.sh"
-        ExecStartPost=systemctl disable wavelet_install_pxe.service
-        [Install]
-        WantedBy=multi-user.target" > /etc/systemd/system/wavelet_install_pxe.service
-    systemctl daemon-reload
-    # From here, the system will reboot.
-    # wavelet_install_depends.service will then run, and force enable wavelet_install_pxe.service
-    # wavelet_pxe_install.service will complete the root portion of the server spinup, allowing build_ug.sh to run in userspace.
-    systemctl enable wavelet_install_depends.service
+[Unit]
+Description=Install PXE support
+ConditionPathExists=/var/wavelet_depends.complete
+After=multi-user.target
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/bash -c "/usr/local/bin/wavelet_pxe_grubconfig.sh"
+ExecStartPost=systemctl disable wavelet_install_pxe.service
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/wavelet_install_pxe.service
+
+	if [[ -f /var/prod.security.enabled ]]; then
+		echo -e "Generating systemd unit for security layer.."
+		echo -e "\
+[Unit]
+Description=Install Security Layer
+ConditionPathExists=/var/prod.security.enabled
+ConditionPathExists=/var/wavelet_depends.complete
+After=multi-user.target
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/bash -c "/usr/local/bin/wavelet_install_hardening.sh"
+ExecStartPost=systemctl disable wavelet_install_hardening.service
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/wavelet_install_hardening.service
+	fi
+	systemctl daemon-reload
+	# From here, the system will reboot.
+	# wavelet_install_depends.service will then run, and force enable wavelet_install_pxe.service
+	# wavelet_pxe_install.service will complete the root portion of the server spinup
+	# OR
+	# It will detect the security layer flag and call wavelet_install_hardening.sh, then reboot allowing build_ug.sh to run in userspace.
+	systemctl enable wavelet_install_depends.service
 }
 
 get_ipValue(){
@@ -93,14 +134,20 @@ get_ipValue(){
 }
 
 determine_ifSurface(){
+	# We already know we're x86, now we check to see if we're a Surface and that extended overlays should be built.
 	# Ref https://github.com/linux-surface/linux-surface
-	echo -e "\nThis is where we would attempt to build an ostree overlay using the surface kernel\n"
 	if [[ ${determination} == 1 ]]; then
-		echo "This device is a surface, proceeding to generate a CoreOS overlay based off the Surface Kernel..\n"
-		# Here we would build a surface ostree container then push it back to the server registry.
+		echo "This device is a surface, proceeding to pull a CoreOS overlay based off the Surface Kernel..\n"
+			if [[ -f /var/extended_x86_support.flag ]]; then
+				echo -e "Extended x86 support is enabled, building surface kernel container overlay..\n"
+				rpm_ostree_surfaceKernel
+			else
+				echo -e "Extended x86 device support is DISABLED.  We will not build additional coreos overlays and utilize the standard overlay instead..\n"
+				rpm_overlay_install_decoder
+			fi
 	else
 		echo -e "This platform has some microsoft identifiers, but does not appear to be a Surface device, reverting to standard ostree overlay.."
-		rpm_overlay_install
+		rpm_overlay_install_decoder
 	fi
 }
 
@@ -166,7 +213,86 @@ EOF
 	# Push image to container registry
 	# N.B can only use --compress with dir: transport method.  
 	podman push localhost:5000/coreos_overlay:latest 192.168.1.32:5000/coreos_overlay --tls-verify=false
-	echo -e "\nRPM package updates completed, finishing installer task..\n"
+	echo -e "\nRPM package updates completed, finishing installer task and checking for extended support..\n"
+}
+
+rpm_ostree_surfaceKernel(){
+	# Surface kernel will require MOK for running a nonstandard kernel.
+	DKMS_KERNEL_VERSION=$(uname -r)
+	# Shamelessly stolen from;
+	# https://github.com/icedream/customizepkg-config/blob/main/decklink.patches/0001-Add-signing-key-generation-post-install-secure-boot-.patch
+	cat > "/home/wavelet/containerfiles/openssl.cnf" << EOF
+HOME        = /var/lib/blackmagic
+RANDFILE    = /var/lib/blackmagic/.rnd
+
+[ req ]
+distinguished_name      = req_distinguished_name
+x509_extensions   = v3_ca
+string_mask       = utf8only
+
+[ req_distinguished_name ]
+
+[ v3_ca ]
+subjectKeyIdentifier    = hash
+authorityKeyIdentifier  = keyid:always,issuer
+basicConstraints  = critical,CA:FALSE
+
+# We use extended key usage information to limit what this auto-generated
+# key can be used for.
+#
+# codeSigning:  specifies that this key is used to sign code.
+#
+# 1.3.6.1.4.1.2312.16.1.2:  defines this key as used for module signing
+#         only. See https://lkml.org/lkml/2015/8/26/741.
+#
+extendedKeyUsage  = codeSigning,1.3.6.1.4.1.2312.16.1.2
+nsComment         = "OpenSSL Generated Certificate"
+EOF
+	echo ':: A certificate to sign the driver has been created at /var/lib/blackmagic/MOK.der. This certificate needs to be enrolled if you run Secure Boot with validation (e.g. shim).'
+	echo -e "\nPlease run:\nmokutil --import '/var/lib/blackmagic/MOK.der'\nIn order to enroll the MOK key!!"
+	podman build -t localhost/coreos_overlay \
+	--build-arg DKMS_KERNEL_VERSION=${DKMS_KERNEL_VERSION} \
+	-v=/home/wavelet/containerfiles:/mount:z \
+	-f /home/wavelet/containerfiles/Containerfile.surface.coreos.overlay
+	podman tag localhost/coreos_overlay localhost:5000/coreos_overlay_surface
+	touch /var/rpm-ostree-overlay.complete
+	touch /var/rpm-ostree-overlay.rpmfusion.repo.complete
+	touch /var/rpm-ostree-overlay.rpmfusion.pkgs.complete
+	# N.B can only use --compress with dir: transport method. zstd would be pretty cool, no? 
+	podman push localhost:5000/coreos_overlay_surface:latest 192.168.1.32:5000/coreos_overlay_surface --tls-verify=false
+}
+
+rpm_ostree_ARM(){
+	# We are building ARM version here, so the containerfile would specify arm-specific libraries.  This would need A LOT of work...
+	if [[ -f /var/arm_support.flag ]];; then
+		echo -e "ARM support is enabled, building ARM OCI overlay and downloading additional UltraGrid build..\n"
+	else
+		echo -e "ARM support is NOT enabled, and we are running on an ARM device.  Exiting.\n"; exit 1
+	fi
+	podman build -t localhost/coreos_overlay \
+	--build-arg DKMS_KERNEL_VERSION=${DKMS_KERNEL_VERSION} \
+	-v=/home/wavelet/containerfiles:/mount:z \
+	-f /home/wavelet/containerfiles/Containerfile.arm.coreos.overlay
+	podman tag localhost/coreos_overlay localhost:5000/coreos_overlay_arm
+	touch /var/rpm-ostree-overlay.complete
+	touch /var/rpm-ostree-overlay.rpmfusion.repo.complete
+	touch /var/rpm-ostree-overlay.rpmfusion.pkgs.complete
+	# N.B can only use --compress with dir: transport method. zstd would be pretty cool, no? 
+	podman push localhost:5000/coreos_overlay_surface:latest 192.168.1.32:5000/coreos_overlay_arm --tls-verify=false
+}
+
+rpm_ostree_RISCV(){
+	# We are building RISCV version here, so the containerfile would specify libraries for RISC-V target platforms.  This would need A LOT of work...
+	podman build -t localhost/coreos_overlay \
+	--build-arg DKMS_KERNEL_VERSION=${DKMS_KERNEL_VERSION} \
+	-v=/home/wavelet/containerfiles:/mount:z \
+	-f /home/wavelet/containerfiles/Containerfile.RISCV.coreos.overlay
+	podman tag localhost/coreos_overlay localhost:5000/coreos_overlay_RISCV
+	touch /var/rpm-ostree-overlay.complete
+	touch /var/rpm-ostree-overlay.rpmfusion.repo.complete
+	touch /var/rpm-ostree-overlay.rpmfusion.pkgs.complete
+	# N.B can only use --compress with dir: transport method. zstd would be pretty cool, no? 
+	podman push localhost:5000/coreos_overlay_surface:latest 192.168.1.32:5000/coreos_overlay_RISCV --tls-verify=false
 }
 
 rpm_overlay_install_decoder(){
@@ -220,8 +346,8 @@ systemctl disable zincati.service --now
 # Debug flag
 # set -x
 exec >/home/wavelet/installer.log 2>&1
-    # Fix AVAHI otherwise NDI won't function correctly, amongst other things;  https://www.linuxfromscratch.org/blfs/view/svn/basicnet/avahi.html
-    # Runs first because it doesn't matter what kind of server/client device, it'll need this.
-    groupadd -fg 84 avahi && useradd -c "Avahi Daemon Owner" -d /run/avahi-daemon -u 84 -g avahi -s /bin/false avahi
-    groupadd -fg 86 netdev
+	# Fix AVAHI otherwise NDI won't function correctly, amongst other things;  https://www.linuxfromscratch.org/blfs/view/svn/basicnet/avahi.html
+	# Runs first because it doesn't matter what kind of server/client device, it'll need this.
+	groupadd -fg 84 avahi && useradd -c "Avahi Daemon Owner" -d /run/avahi-daemon -u 84 -g avahi -s /bin/false avahi
+	groupadd -fg 86 netdev
 detect_self
