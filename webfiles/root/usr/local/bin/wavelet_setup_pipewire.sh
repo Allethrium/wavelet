a910fdc5dca0b172e68ead4b1652e57bb564ca88
armelvil <103518994+armelvil@users.noreply.github.com>
2024-11-22 18:16:05 -0500

    Update wavelet_setup_pipewire.sh
    - switch etcd calls to the etcd_interaction module

--- webfiles/root/usr/local/bin/wavelet_setup_pipewire.sh
+++ webfiles/root/usr/local/bin/wavelet_setup_pipewire.sh
@@ -2,10 +2,48 @@
 # This module sets the pipewire audio source to be the same as the video source, or a best guess.
 # Would it make more sense with the new webUI to try and do this with an audio togglebutton?
 
+# Etcd Interaction hooks (calls wavelet_etcd_interaction.sh, which more intelligently handles security layer functions as necessary)
+read_etcd(){
+	printvalue=$(/usr/local/bin/wavelet_etcd_interaction.sh "read_etcd" ${KEYNAME})
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)\n"
+}
+read_etcd_global(){
+	printvalue=$(/usr/local/bin/wavelet_etcd_interaction.sh "read_etcd_global" "${KEYNAME}") 
+	echo -e "Key Name {$KEYNAME} read from etcd for Global Value $printvalue\n"
+}
+read_etcd_prefix(){
+	printvalue=$(/usr/local/bin/wavelet_etcd_interaction.sh "read_etcd_prefix" "${KEYNAME}")
+	echo -e "Key Name {$KEYNAME} read from etcd for value $printvalue for host $(hostname)\n"
+}
+read_etcd_clients_ip() {
+	return_etcd_clients_ip=$(/usr/local/bin/wavelet_etcd_interaction.sh "read_etcd_clients_ip")
+}
+read_etcd_clients_ip_sed() {
+	# We need this to manage the \n that is returned from etcd.
+	# the above is useful for generating the reference text file but this parses through sed to string everything into a string with no newlines.
+	processed_clients_ip=$(/usr/local/bin/wavelet_etcd_interaction.sh "read_etcd_clients_ip" | sed ':a;N;$!ba;s/\n/ /g')
+}
+write_etcd(){
+	/usr/local/bin/wavelet_etcd_interaction.sh "write_etcd" "${KEYNAME}" "${KEYVALUE}"
+	echo -e "Key Name ${KEYNAME} set to ${KEYVALUE} under /$(hostname)/\n"
+}
+write_etcd_global(){
+	/usr/local/bin/wavelet_etcd_interaction.sh "write_etcd_global" "${KEYNAME}" "${KEYVALUE}"
+	echo -e "Key Name ${KEYNAME} set to ${KEYVALUE} for Global value\n"
+}
+write_etcd_client_ip(){
+	/usr/local/bin/wavelet_etcd_interaction.sh "write_etcd_client_ip" "${KEYNAME}" "${KEYVALUE}"
+}
+delete_etcd_key(){
+	/usr/local/bin/wavelet_etcd_interaction.sh "delete_etcd_key" "${KEYNAME}"
+}
+
 
 etcd_get_currentvideosource(){
-	currentInputHash=$(etcdctl --endpoints=192.168.1.32:2379 get "uv_hash_select")
-	long_interface_path=$(etcdctl  --endpoints=192.168.1.32:2379 get "/hash/${currentInputHash}")
+	KEYNAME="uv_hash_select"; read_etcd_global
+	currentInputHash=${printvalue}
+	KEYNAME="/hash/${currentInputHash}"; read_etcd_global
+	long_interface_path=${printvalue}
 	# use awk to clean long_interface var for matching
 	# this should give us Magewell USB Capture, Magewell USB Capture+, Logitech Screen Share or IPEVO Ziggi, or whatever else we get
 	return long_interface_var_cleaned
