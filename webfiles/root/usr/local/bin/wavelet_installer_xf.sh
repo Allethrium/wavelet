cb64ab9c4ff5dc2a9f51aa8e88da82967ab8a82f
armelvil <103518994+armelvil@users.noreply.github.com>
2024-11-22 20:59:58 -0500

    Update wavelet_installer_xf.sh
    - fix etcd container service and put in appropriate folder that is actually writable.  duh.

--- webfiles/root/usr/local/bin/wavelet_installer_xf.sh
+++ webfiles/root/usr/local/bin/wavelet_installer_xf.sh
@@ -109,26 +109,23 @@ WantedBy=multi-user.target" > /etc/systemd/system/wavelet_install_hardening.serv
 	# Quadlet Etcd service, I have decided it's more appropriate to move this back to a system service
 	# This is because it would only run when a user is logged on
 	# We will likely want to secure the server console in prod deployment.
-	echo -e "
-[Unit]
+	mkdir -p /var/etcd-data
+	echo -e "[Unit]
 Description=etcd service
 Documentation=https://github.com/etcd-io/etcd
 
 [Container]
 Image=quay.io/coreos/etcd:v3.5.9
-ContainerName=etcd
+ContainerName=etcd-container
 Network=host
 Volume=/var/etcd-data:/etcd-data:Z
-Environment=/etc/etcd.conf
-ReadOnly=true
 AutoUpdate=registry
 NoNewPrivileges=true
-Exec=/usr/local/bin/etcd
 
 [Install]
-WantedBy=multi-user.target" > /usr/share/containers/systemd/etcd.container
+WantedBy=multi-user.target" > /etc/containers/systemd/etcd-container.container
 	systemctl daemon-reload
-	systemctl enable etcd.service
+	systemctl enable etcd-container.service --now
 	# wavelet_install_depends.service will then run, and force enable wavelet_install_pxe.service
 	# wavelet_pxe_install.service will complete the root portion of the server spinup
 	# OR
