7e287ff80ca32aa1b8021cf6902293b5dfefbd2e
armelvil <103518994+armelvil@users.noreply.github.com>
2024-11-22 20:16:29 -0500

    Update wavelet_detectv4l.sh
    - clean up new etcd interaction (pending testing)

--- webfiles/root/usr/local/bin/wavelet_detectv4l.sh
+++ webfiles/root/usr/local/bin/wavelet_detectv4l.sh
@@ -82,8 +82,7 @@ generate_device_info() {
 	echo -e "generated device hash: $deviceHash \n"
 	device_string_short=$(echo "${device_string_long}" | sed 's/.*usb-//')
 	# Let's look for the device hash in the /interface prefix to make sure it doesn't already exist!
-	KEYNAME="/hash/${deviceHash}"
-	output_return=$(read_etcd_global)
+	KEYNAME="/hash/${deviceHash}"; read_etcd_global; output_return=${printvalue}
 	if [[ $output_return == "" ]] then
 		echo -e "\n${deviceHash} not located within etcd, assuming we have a new device and continuing with process to set parameters..\n"
 		isDevice_input_or_output
@@ -111,34 +110,23 @@ set_device_input() {
 	# populated device_string_short with hash value, this is used by the interface webUI component
 	# device_string_short is effectively the webui Label / banner text.
 	# Because we cannot query etcd by keyvalue, we must create a reverse lookup prefix for everything we want to be able to clean up!!
-	KEYNAME="/interface/$(hostname)/${device_string_short}"
-	KEYVALUE="${deviceHash}"
-	write_etcd_global	
+	KEYNAME="/interface/$(hostname)/${device_string_short}"; KEYVALUE="${deviceHash}"; write_etcd_global	
 	# And the reverse lookup prefix - N.B this is updated from set_label.php when the webUI changes a device label/banner string! 
-	KEYNAME="/short_hash/${deviceHash}"
-	KEYVALUE=$(hostname)/${device_string_short}
-	write_etcd_global
+	KEYNAME="/short_hash/${deviceHash}"; KEYVALUE=$(hostname)/${device_string_short}; write_etcd_global
 	# We need this to perform cleanup "gracefully"
-	KEYNAME="/long_interface${device_string_long}"
-	KEYVALUE=${deviceHash}
-	write_etcd_global
+	KEYNAME="/long_interface${device_string_long}"; KEYVALUE=${deviceHash}; write_etcd_global
 	# This will enable us to find the device from its hash value, along with the registered host encoder, like a reverse DNS lookup..
 	# GLOBAL value\
 	echo -e "Attempting to set keyname ${deviceHash} for $(hostname)${device_string_long}"
 	KEYNAME="/hash/${deviceHash}"
 	# Stores the device data under hostname/inputs/device_string_long
-	KEYVALUE="/$(hostname)/inputs${device_string_long}"
-	write_etcd_global
+	KEYVALUE="/$(hostname)/inputs${device_string_long}"; write_etcd_global
 	# notify watcher that input device configuration has changed
-	KEYNAME=new_device_attached
-	KEYVALUE=1
-	write_etcd_global
+	KEYNAME=new_device_attached; KEYVALUE=1; write_etcd_global
 	echo -e "resetting variables to null."
 	deviceHash=""
 	device_string_short=""
-	KEYNAME=INPUT_DEVICE_PRESENT
-	KEYVALUE=1
-	write_etcd
+	KEYNAME=INPUT_DEVICE_PRESENT; KEYVALUE=1; write_etcd
 	detect
 }
 
@@ -159,7 +147,7 @@ declare -a interfaceLongArray=$(read_etcd_keysonly | sed 's/\/long_interface// '
 		echo -e "Orphaned devices located: \n"
 			printf "%s\n" "${leftOversArray[@]}"
 		for i in "${leftOversArray[@]}"
-					do
+			do
 				cleanupStringLong=$i
 				echo -e "\n\nCleanup device is ${cleanupStringLong}"
 				# delete the input caps key for the missing device
@@ -176,14 +164,14 @@ declare -a interfaceLongArray=$(read_etcd_keysonly | sed 's/\/long_interface// '
 				KEYNAME="/hash/${cleanupHash}"; delete_etcd_key
 				
 				# finally, find and delete from interface prefix - Guess we need ANOTHER lookup table to manage to keep all of this straight..
-				KEYNAME="/short_hash/${cleanupHash}"; cleanupInterface=$(read_etcd_global)
+				KEYNAME="/short_hash/${cleanupHash}"; read_etcd_global; cleanupInterface=${printvalue}
 				echo -e "Device UI Interface label located in /short_hash/${cleanupHash} for the value ${cleanupInterface}"
 				echo -e "Deleting /short_hash/${cleanupHash}  entry"
 				delete_etcd_key
 				echo -e "Deleting /interface/${cleanupInterface} entry"
 				KEYNAME="/interface/$${cleanupInterface}"; delete_etcd_key
 				echo -e "Device entry ${cleanupStringLong} should be removed along with all references to ${cleanupHash}\n\n"
-					done
+			done
 	fi
 }
 
@@ -215,53 +203,40 @@ detect() {
 event_ipevo() {
 # each of these blocks contains specific configuration options that must be preset for each device in order for them to work.  
 # We will have to add to this over time to support more devices appropriately.
-# Specifically this camera supports MJPG so we will use that instead of YUYV for the capture pixel format
-# 08/07/24 - and today all of a sudden MJPG->native stopped working, so we are back to convertRBG...
+# 
 	echo -e "IPEVO Camera detection running..\n"
-	KEYVALUE="-t v4l2:codec=MJPG:convert=RGB:size=1920x1080:tpf=1/30:device=${v4l_device_path}"
-	KEYNAME=${device_string_long}
-	write_etcd_inputs
-	echo -e "\n Detection completed for IPEVO device.. \n \n \n \n"
+	KEYNAME="/inputs/${device_string_long}"; KEYVALUE="-t v4l2:codec=MJPG:convert=RGB:size=1920x1080:tpf=1/30:device=${v4l_device_path}"; write_etcd
+	echo -e "\nDetection completed for IPEVO device..\n"
 	device_cleanup
 }
 event_logitech_hdmi() {
-	KEYNAME=${device_string_long}
-	KEYVALUE="-t v4l2:codec=MJPEG:convert=RGB:size=1920x1080:tpf=1/30:device=${v4l_device_path}"
-	write_etcd_inputs
-	echo -e "\n Detection completed for Logitech HDMI Capture device.. \n \n \n \n"
+	KEYNAME="/inputs/${device_string_long}"; KEYVALUE="-t v4l2:codec=MJPEG:convert=RGB:size=1920x1080:tpf=1/30:device=${v4l_device_path}"; write_etcd
+	echo -e "\nDetection completed for Logitech HDMI Capture device..\n"
 	device_cleanup
 }
 event_magewell() {
 	echo -e "Setting up Magewell USB capture card..\n"
-	KEYVALUE="-t v4l2:codec=YUYV:size=1920x1080:tpf=1/30:convert=RGB:device=${v4l_device_path}"
-		KEYNAME="${device_string_long}"
-	write_etcd_inputs
-	echo -e "\n Detection completed for device.. \n \n \n \n"
+	KEYNAME="/inputs/${device_string_long}"; KEYVALUE="-t v4l2:codec=YUYV:size=1920x1080:tpf=1/30:convert=RGB:device=${v4l_device_path}"; write_etcd
+	echo -e "\nDetection completed for device..\n"
 	device_cleanup
 }
 event_epson() {
 	echo -e "Setting up EPSON Document camera device...\n"
-	KEYVALUE="-t v4l2:codec=MJPG:size=1920x1080:tpf=1/24:device=${v4l_device_path}"
-		KEYNAME="${device_string_long}"
-	write_etcd_inputs
-	echo -e "\n Detection completed for device.. \n \n \n \n"
+	KEYNAME="/inputs/${device_string_long}"; KEYVALUE="-t v4l2:codec=MJPG:size=1920x1080:tpf=1/24:device=${v4l_device_path}"; write_etcd
+	echo -e "\nDetection completed for device..\n"
 	device_cleanup
 }
 event_dellWB3023(){
 	echo -e "Setting up Dell WB3023 webcam..\n"
-	KEYVALUE="-t v4l2:codec=MJPG:size=640x480:tpf=1/30:device=${v4l_device_path}"
-		KEYNAME="${device_string_long}"
-	write_etcd_inputs
-	echo -e "\n Detection completed for device.. \n \n \n \n"
+	KEYNAME=KEYNAME="/inputs/${device_string_long}"; KEYVALUE="-t v4l2:codec=MJPG:size=640x480:tpf=1/30:device=${v4l_device_path}"; write_etcd
+	echo -e "\nDetection completed for device..\n"
 	device_cleanup	
 }
 event_unknowndevice() {
 # 30fps is a compatibility setting, catch all for other devices we will leave at 30.  Try YUYV with RGB conversion..
 	echo -e "The connected device has not been previously assigned an input ID for the UI component.  Storing hash.\n"
-	KEYVALUE="-t v4l2:codec=YUYV:size=1920x1080:tpf=1/30:convert=RGB:device=${v4l_device_path}"
-		KEYNAME="${device_string_long}"
-	write_etcd_inputs
-	echo -e "\n Detection completed for device.. \n \n \n \n"
+	KEYNAME=KEYNAME="/inputs/${device_string_long}"; KEYVALUE="-t v4l2:codec=YUYV:size=1920x1080:tpf=1/30:convert=RGB:device=${v4l_device_path}"; write_etcd
+	echo -e "\n Detection completed for device..\n"
 	device_cleanup
 }
 
@@ -271,14 +246,10 @@ event_unknowndevice() {
 event_biamp() {
 # This should set the biAmp up as a Pipewire audio output sink by default, so Wavelet will stream any audio data the system receives into the biAmp
 	echo -e "biAmp detection running..\n"
-	KEYVALUE="testValue"
-	KEYNAME=${device_string_long}
-	write_etcd_inputs
+	KEYNAME="/inputs/${device_string_long}"; KEYVALUE="???"; KEYNAME=${device_string_long}; write_etcd
 	# Set AUDIO_OUT flag so the UI does not generate an input button, but an audio OUT button instead?
-	KEYVALUE="1"
-	KEYNAME=${device_string_long}/AUDIO_OUT
-	write_etcd_inputs
-	echo -e "\n Detection completed for IPEVO device.. \n \n \n \n"
+	KEYVALUE="1"; KEYNAME=${device_string_long}/AUDIO_OUT; write_etcd
+	echo -e "\n Detection completed for BiAmp audio device..\n"
 	device_cleanup
 }
 
@@ -336,13 +307,11 @@ exec >/home/wavelet/detectv4l.log 2>&1
 if [[ "${1}" = "RD" ]]; then
 	echo -e "Called by refresh devices, hanging watcher for two seconds whilst we reset the key.."
 	systemctl --user disable wavelet_device_redetect.service --now
-	KEYNAME="DEVICE_REDETECT"
-	KEYVALUE="0"
-	write_etcd_global
+	KEYNAME="DEVICE_REDETECT"; KEYVALUE="0"; write_etcd_global
 	sleep 2
 	systemctl --user enable wavelet_device_redetect.service --now
 fi
 
 # check to see if I'm a server or an encoder
-echo -e "\n \n \n ********Begin device detection and registration process...******** \n \n \n"
+echo -e "\n\n\n********Begin device detection and registration process...********\n\n\n"
 detect_self
