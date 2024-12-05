2a9b0d94118f152ce991240db289a7439ea769f6
armelvil <103518994+armelvil@users.noreply.github.com>
2024-12-05 10:23:23 -0500

    Update wavelet_install_ug_depends.sh
    - remove old comment

--- webfiles/root/usr/local/bin/wavelet_install_ug_depends.sh
+++ webfiles/root/usr/local/bin/wavelet_install_ug_depends.sh
@@ -98,9 +98,6 @@ rpm_ostree_install_git(){
 	# Needed because otherwise sway launches the userspace setup before everything is ready
 	# Some other packages which do not install properly in the ostree container build are also included here
 	/usr/bin/rpm-ostree install -y -A git
-	# We have to install avahi this way because the ostree overlay does NOT install dependencies correctly, and it's REQUIRED for NDI to function.
-	# --from repo='updates' necessary, otherwise rpm-ostree complains about non-local overrides not being implemented.
-	#/usr/bin/rpm-ostree override replace avahi --experimental --from repo='updates'
 }
 
 rpm_ostree_install(){
