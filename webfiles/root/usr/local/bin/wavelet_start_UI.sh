58b34621878b22e53650a8a33e40469ef06e1a02
armelvil <103518994+armelvil@users.noreply.github.com>
2024-12-19 10:45:12 -0500

    Update wavelet_start_UI.sh
    - kiosk mode doesn't work well in Sway, oh well
    - add logging

--- webfiles/root/usr/local/bin/wavelet_start_UI.sh
+++ webfiles/root/usr/local/bin/wavelet_start_UI.sh
@@ -4,6 +4,26 @@
 # Obviously won't work without reliable DNS resolution
 # Might grow if we can finally work out why FireFox is ignoring the policies.json file, or maybe we just dump it and move to LibreWolf.
 
-serverIP=$(nslookup svr | awk '/^Address: / { print $2 }')
-sleep 3
-exec firefox --kiosk http://${serverIP}:9080
+main(){
+	serverIP=$(nslookup svr | awk '/^Address: / { print $2 }')
+	sleep 3
+	exec firefox http://${serverIP}:9080
+}
+
+#####
+#
+# Main
+#
+#####
+
+logName=/var/home/wavelet/webui.log
+if [[ -e $logName || -L $logName ]] ; then
+        i=0
+        while [[ -e $logName-$i || -L $logName-$i ]] ; do
+                let i++
+        done
+        logName=$logName-$i
+fi
+
+#set -x
+exec > "${logName}" 2>&1
