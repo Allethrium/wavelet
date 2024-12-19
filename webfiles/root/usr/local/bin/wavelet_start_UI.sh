8b96efb8aba49d14c54af1fb720fb2b1b276395c
armelvil <103518994+armelvil@users.noreply.github.com>
2024-12-19 09:45:46 -0500

    Update wavelet_start_UI.sh
    - fix IP detection for server

--- webfiles/root/usr/local/bin/wavelet_start_UI.sh
+++ webfiles/root/usr/local/bin/wavelet_start_UI.sh
@@ -2,7 +2,8 @@
 # Simple module which adds a delay to firefox 
 # Finds server IP address and adds it to the UI Argument
 # Obviously won't work without reliable DNS resolution
+# Might grow if we can finally work out why FireFox is ignoring the policies.json file, or maybe we just dump it and move to LibreWolf.
 
-nslookup svr | awk '/^Address: / { print $2 }'
-sleep 5
+serverIP=$(nslookup svr | awk '/^Address: / { print $2 }')
+sleep 3
 exec firefox --kiosk http://${serverIP}:9080
