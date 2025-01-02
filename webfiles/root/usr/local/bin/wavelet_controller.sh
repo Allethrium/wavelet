2efe2ea0f8857b0962c279349899eb0fd654124f
armelvil <103518994+armelvil@users.noreply.github.com>
2025-01-02 11:59:03 -0500

    Update wavelet_controller.sh
    - now checks for and handles banner status in order to notify encoder if a restart is required.  This would be necessary for the banner contents to change, which effectively means we'd be stuck with the old terminate-restart approach.

--- webfiles/root/usr/local/bin/wavelet_controller.sh
+++ webfiles/root/usr/local/bin/wavelet_controller.sh
@@ -1,34 +1,19 @@
-
-
 #!/bin/bash
 #
 # The controller is responsible for orchestrating the rest of the system
 
-
-# Define standard default variables for encoders
-uv_videoport="5004"
-uv_audioport="5006"
-uv_reflector="192.168.1.32"
-uv_islivestreaming="0"
 
 ###
 #
 # Routine codeblocks that define things which this script handles.
 #
 ###
+
 detect_self(){
 # Controller only runs on the server.
 UG_HOSTNAME=$(hostname)
 	echo -e "Hostname is $UG_HOSTNAME \n"
 	case $UG_HOSTNAME in
-	enc*) 			echo -e "I am an Encoder \n" && echo -e "Cannot run the controller on an encoder, exiting..";	exit 0
-	;;
-	dec*)			echo -e "I am a Decoder \n" && echo -e "Cannot run the controller on a decoder, exiting..";	exit 0
-	;;
-	livestream*)	echo -e "I am a Livestreamer \n" && echo -e "Cannot run the controller on a livestreamer, exiting..";	exit 0
-	;;
-	gateway*)		echo -e "I am an input Gateway for another video streaming system \n" && echo -e "Cannot run the controller on a gateway, exiting..";	exit 0
-	;;
 	svr*)			echo -e "I am a Server. Proceeding..." && event_server
 	;;
 	*) 				echo -e "This device Hostname is not set approprately, exiting \n" &&	exit 0
@@ -36,26 +21,22 @@ UG_HOSTNAME=$(hostname)
 	esac
 }
 
-
 event_server(){
 echo -e "\n\n***Controller Called, checking input key and acting accordingly..***"
-# Now called by etcd so inputting standard values to etcd would overwrite everything every time an event happened.  
-# These are populated by wavelet_init.sh
 main
 }
-
 
 main() {
 	KEYNAME=input_update; read_etcd_global
 	if [[ "${printvalue}" == 1 ]]; then
-		echo -e "\ninput_update key is set to 1, continuing with task.. \n"
+		echo -e "\ninput_update key is set to 1, continuing with task..\n"
 	else
-		echo -e "\ninput_update key is set to 0, doing nothing.. \n"
+		echo -e "\ninput_update key is set to 0, doing nothing..\n"
 		exit 0
 	fi
-	KEYNAME=uv_hash_select; read_etcd_global; event=${printvalue}
+	KEYNAME="uv_hash_select"; read_etcd_global; event="${printvalue}"
 	# Check livestream toggle UI value
-	KEYNAME=/livestream/enabled; read_etcd_global; livestream_state=${printvalue}
+	KEYNAME="/livestream/enabled"; read_etcd_global; livestream_state="${printvalue}"
 		if [[ "${livestream_state}" = 0 ]]; then
 			echo "Livestreaming is off, setting LiveStreaming flag to disabled"
 			KEYNAME=uv_islivestreaming; KEYVALUE="0"; write_etcd_global
@@ -63,13 +44,17 @@ main() {
 			echo "Livestreaming is on, setting LiveStreaming flag to enabled"
 			KEYNAME=uv_islivestreaming; KEYVALUE="1"; write_etcd_global
 		fi
+	bannerActive=0
+	KEYNAME="/banner/enabled"; read_etcd_global
+	if [[ ${printvalue} == "1" ]]; then
+		bannerActive=${printvalue}
+	fi
 	waveletcontroller
 }
 
 
 waveletcontroller() {
 # Tests event input and runs appropriate event
-# 11/2023 - note that hardcoded inputs are no longer used here, the case $event in line just tests static buttons from the webUI.  The rest is handled between detectv4l and wavelet_encoder, for the most part.
 case $event in
 	# 1
 	(1) 	echo -e "Option One, Blank activated\n"						;current_event="wavelet-blank"			;wavelet_blank								;;
@@ -87,11 +72,6 @@ case $event in
 	(SR)	echo -e "Whole system reboot\n"								;current_event="wavelet-system-reboot"	;wavelet_system_reboot						;;
 	(CL)	echo -e "Clearing All Input Sources from keystore..\n"		;current_event="wavelet-clear-inputs"	;wavelet_clear_inputs						;;
 	(RD)	echo -e "Running re-detection of source devices..\n"		;current_event="wavelet-detect-inputs"	;wavelet_refresh							;;
-#	if [ $recording = true ]; then
-#		echo "Recording to archive file" && recording=true && wavelet_record_start
-#	if [ $recording = false ]; then
-#		($false) echo "Recording to archive file" && recording=true && wavelet_record_start;; 
-	# does not kill any streams, instead copies stream and appends to a labeled MKV file (not implemented unless we get a real server w/ STORAGE)
 	# HW and SW modes selected for compatibility reasons - some decoders don't like HW encoded video.  SW encoding will need a *FAST* CPU unless you like latency, dropped frames and glitches.
 	(A)		event_x264sw						&& echo "x264 Software video codec selected, updating encoder variables"						;;
 	(B)		event_x264hw 						&& echo "x264 VA-API video codec selected, updating encoder variables"							;;
@@ -114,11 +94,11 @@ case $event in
 	#
 	# Multiple input modes go here (I wonder if there's a better, matrix-based approach to this?)
 	#
-	(W) echo "Four-way panel split activated \n"						;current_event="event_foursplit";wavelet_foursplit						;;
-	(X) echo "Two-way panel split activated \n"							;current_event="event_twosplit"	;wavelet_twosplit						;;
-	(Y) echo "Picture-in-Picture 1 activated \n"						;current_event="event_pip1"		;wavelet_pip1							;;
-	(Z) echo "Picture-in-Picture 2 activated \n"						;current_event="event_pip2"		;wavelet_pip2							;;
-	(*) echo "Unknown predefined input, passing hash to encoders.. \n"	;current_event="dynamic"		;wavelet_dynamic					;;
+	(W) echo "Four-way panel split activated"						;current_event="event_foursplit";wavelet_foursplit						;;
+	(X) echo "Two-way panel split activated"						;current_event="event_twosplit"	;wavelet_twosplit						;;
+	(Y) echo "Picture-in-Picture 1 activated"						;current_event="event_pip1"		;wavelet_pip1							;;
+	(Z) echo "Picture-in-Picture 2 activated"						;current_event="event_pip2"		;wavelet_pip2							;;
+	(*) echo "Unknown predefined input, passing hash to encoders.."	;current_event="dynamic"		;wavelet_dynamic						;;
 esac
 }
 
@@ -207,8 +187,6 @@ wavelet_seal() {
 	cd /home/wavelet/
 	current_event="wavelet-seal"
 	KEYNAME=uv_input; KEYVALUE="SEAL"; write_etcd_global
-	# Write server-local encoder restart key
-	#KEYNAME="encoder_restart"; KEYVALUE="1"; write_etcd
 	# We now use the switcher for simple things
 	echo 'capture.data 1' | busybox nc -v 127.0.0.1 6160
 	echo -e "\nStatic image activated from server encoder..\n"
@@ -219,8 +197,6 @@ wavelet_testcard() {
 # Test Card
 	current_event="wavelet-testcard"
 	KEYNAME=uv_input; KEYVALUE="BLANK";	write_etcd_global
-	# Write server-local encoder restart key
-	#KEYNAME="encoder_restart"; KEYVALUE="1"; write_etcd
 	echo 'capture.data 2' | busybox nc -v 127.0.0.1 6160
 }
 
@@ -284,6 +260,23 @@ set_channelIndex(){
 		sleep 3; KEYNAME=input_update; KEYVALUE="1"; write_etcd_global
 	fi
 
+	# And check for the banner, because that would need an encoder restart also
+	echo "Banner status is: ${bannerActive}"
+	KEYNAME=uv_filter_cmd; read_etcd_global; filterVar=$(echo ${printvalue} | base64 -d)
+	echo "filterVar is: ${filterVar}"
+	if [[ ${bannerActive} == 1 ]]; then
+		KEYNAME="encoder_restart"; KEYVALUE="1"; write_etcd
+		sleep 3
+	elif [[ ${bannerActive} == 0 && -n "${filterVar}" ]]; then
+		echo "Banner disabled but capture filter populated, this means we previously had the banner on and we need to remove it."
+		KEYNAME="encoder_restart"; KEYVALUE="1"; write_etcd
+		KEYNAME="uv_filter_cmd"; delete_etcd_key_global
+		sleep 3		
+	else
+		echo "Banner not enabled and capture filter arg is null, doing nothing."
+		:
+	fi
+
 	# And now set the appropriate channel
 	echo "Channel Index is: ${channelIndex%,*}"
 	echo "capture.data ${channelIndex%,*}" | busybox nc -v ${targetHost} 6160	
@@ -337,13 +330,15 @@ wavelet_foursplit() {
 	KEYNAME=uv_input; KEYVALUE="Multi source mix"; write_etcd_global
 	#controllerInputLabel=${printvalue}
 	KEYNAME=uv_hash_select; read_etcd_global; controllerInputHash=${printvalue}
-	echo -e "\n \n Controller notified that the Four-way split input hash has been selected from the WebUI.  Encoder will do its best to generate a software mix of up to four available input devices. \n \n "
+	echo -e "\n\nController notified that the Four-way split input hash has been selected from the WebUI.\nEncoder will do its best to generate a software mix of up to four available input devices.\n\n "
 	# Kill existing streaming on the SERVER
 	systemctl --user stop UltraGrid.AppImage.service
 	# Set encoder restart flag to 1
 	KEYNAME=encoder_restart; KEYVALUE="1"; write_etcd_global
 	KEYNAME=input_update; KEYVALUE="0"; echo -e "\n Task completed, resetting input_update key to 0.. \n"; write_etcd_global
 }
+
+
 # These events contain additional codec-specific settings that have been found to work acceptably well on the system.
 # Since they are tuned by hand, you probably won't want to modify them unless you know exactly what you're doing.
 # Proper operation depends on bandwidth, latency, network quality, encoder speed.  It's highly hardware dependent.
