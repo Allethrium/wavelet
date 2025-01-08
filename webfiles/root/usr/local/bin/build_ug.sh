6b7bd098b32a7ebe5c4fdfa70bffb7bee69a8d43
armelvil <103518994+armelvil@users.noreply.github.com>
2025-01-08 12:48:39 -0500

    Update build_ug.sh
    - rise start limit burst as it can still get triggered if someone gets click-happy on the ui

--- webfiles/root/usr/local/bin/build_ug.sh
+++ webfiles/root/usr/local/bin/build_ug.sh
@@ -491,7 +491,7 @@ Wants=network-online.target
 [Service]
 ExecStart=/bin/bash -c "/usr/local/bin/run_ug.sh"
 StartLimitIntervalSec=3
-StartLimitBurst=12
+StartLimitBurst=30
 
 [Install]
 WantedBy=default.target" > /var/home/wavelet/.config/systemd/user/run_ug.service
