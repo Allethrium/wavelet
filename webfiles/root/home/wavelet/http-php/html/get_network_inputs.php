d095762c9ff371ce1d6dcdd5639af9635e0e1e52
armelvil <103518994+armelvil@users.noreply.github.com>
2025-02-03 14:34:37 -0500

    Update get_network_inputs.php
    - move to /UI/

--- webfiles/root/home/wavelet/http-php/html/get_network_inputs.php
+++ webfiles/root/home/wavelet/http-php/html/get_network_inputs.php
@@ -17,10 +17,10 @@ function poll_etcd_inputs($keyPrefix, $keyPrefixPlusOneBit) {
 	curl_close($ch);
 	$dataArray = json_decode($result, true);
 	foreach ($dataArray['kvs'] as $x => $item) {
-		$decodedKeyShort = (str_replace("-video-index0", "", (str_replace("/network_interface/short/", "", (base64_decode($item['key']))))));
+		$decodedKeyShort = (str_replace("-video-index0", "", (str_replace("/UI/network_interface/short/", "", (base64_decode($item['key']))))));
 		$decodedKey = base64_decode($item['key']);
 		$decodedValue = base64_decode($item['value']);
-		$decodedShortHash = (str_replace("/network_interface/", "", base64_decode($item['value'])));
+		$decodedShortHash = (str_replace("/UI/network_interface/", "", base64_decode($item['value'])));
 		$ipValue=get_device_ip($decodedShortHash);
 			$newData[] = [
 				'value' => $decodedValue,
@@ -57,8 +57,8 @@ function get_device_ip($decodedShortValue){
 }
 
 // modify these strings if you want to get a different key prefix out of etcd.  For Wavelet, there's no reason to change them.
-$prefixstring = '/network_interface/';
-$prefixstringplusone = '/network_interface0';
+$prefixstring = '/UI/network_interface/';
+$prefixstringplusone = '/UI/network_interface0';
 $keyPrefix=base64_encode($prefixstring);
 $keyPrefixPlusOneBit=base64_encode($prefixstringplusone);
 poll_etcd_inputs($keyPrefix, $keyPrefixPlusOneBit);
