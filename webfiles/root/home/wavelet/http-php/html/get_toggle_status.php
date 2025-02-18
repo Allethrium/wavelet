067592f5857db12764881a050a99093b4240faec
armelvil <103518994+armelvil@users.noreply.github.com>
2025-02-18 14:29:08 -0500

    Update get_toggle_status.php
    - WIP update w/ auth test

--- webfiles/root/home/wavelet/http-php/html/get_toggle_status.php
+++ webfiles/root/home/wavelet/http-php/html/get_toggle_status.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-type: application/json');
+include('get_auth_token.php');
+
 // this module handles all the toggle functions in the webUI.  It takes one argument (the toggle key)
 
 // Import the etcd password from the OS environment
@@ -7,24 +9,6 @@ $password = base64_encode('test');
 $username = base64_encode('wavelet_webui');
 
 $toggleKey = $_POST["key"];
-
-
-function get_etcd_authtoken($password, $username) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-	// returns etcd authorization token utilizing the systemd ETCD_PASS variable and the username wavelet_webui
-	curl_setopt($ch, CURLOPT_URL, 'http://1982.168.1.32:2379/v3/auth/authenticate');
-	$headers = [
-		'Content-Type: application/x-www-form-urlencoded',
-	];
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"name\": \"$username\", \"password\": \"$password\"}");
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	$token = curl_exec($ch);
-	curl_close($ch);
-	return $token;
-}
 
 function poll_etcd_inputs($token, $keyPrefix) {
 	$ch = curl_init();
@@ -58,5 +42,6 @@ error_reporting(E_ALL);
 $prefixstring = "/ui/$toggleKey";
 $keyPrefix=base64_encode($prefixstring);
 $token=get_etcd_authtoken($password, $username);
+$token=get_etcd_authtoken;
 poll_etcd_inputs($token, $keyPrefix);
 ?>
