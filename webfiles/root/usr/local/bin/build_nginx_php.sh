0c609210476fe0dd919f80bdb4dfbb959dbaad10
armelvil <103518994+armelvil@users.noreply.github.com>
2025-02-18 13:58:40 -0500

    Update build_nginx_php.sh
    - fix cert generation and move ports to traditional http(s) ports.

--- webfiles/root/usr/local/bin/build_nginx_php.sh
+++ webfiles/root/usr/local/bin/build_nginx_php.sh
@@ -26,16 +26,14 @@ podman_quadlet(){
 	# The .kube file at the end basically allows us to link these two services into a podman pod.   
 	# The install wantedBy= section is how we do systemctl enable --now, basically.
 	mkdir -p /var/home/wavelet/.config/containers/systemd/
-	echo -e "
-[Unit]
+	echo -e "[Unit]
 Description=PHP + FPM
 [Container]
 Image=docker.io/library/php:fpm
 AutoUpdate=registry
 Pod=http-php.pod" > /var/home/wavelet/.config/containers/systemd/php-fpm.container
 # For Nginx, ports are mapped to 9080 and 9443 respectively..
-	echo -e "
-[Unit]
+	echo -e "[Unit]
 Description=NGINX
 [Container]
 Image=docker.io/library/nginx:alpine
@@ -45,20 +43,21 @@ Pod=http-php.pod" > /var/home/wavelet/.config/containers/systemd/nginx.container
 	echo -e "Security layer enabled, adding mounts for certificates..\n"
 		# Check for prod certs
 		if [[ ! -f /etc/pki/tls/certs/http.crt ]]; then
-			# we generate a crappy certificate so things work, at the very least..
+			# Since prod certs aren't there, we generate a crappy certificate so things work, at the very least..
+			# wait, this won't work because we cannot write to the directory.
 			echo -e "Certificate has not been generated on server, generating a snake oil certificate for testing..\n"
-			openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/pki/tls/certs/httpd.key -out /etc/pki/tls/certs/httpd.crt -subj "/C=US/ST=NewYork/L=NewYork/O=ALLETHRIUM/OU=DevOps/CN=WaveletInterface"
-			openssl dhparam -out /etc/pki/certs/dhparam.pem 4096
+			mkdir -p /var/home/wavelet/http-php/certs
+			openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /var/home/wavelet/http-php/certs/httpd.key -out /var/home/wavelet/http-php/certs/httpd.crt -subj "/C=US/ST=NewYork/L=NewYork/O=ALLETHRIUM/OU=DevOps/CN=WaveletInterface"
+			openssl dhparam -out /var/home/wavelet/http-php/certs/dhparam.pem 4096
 		fi
 		# Cert directory mounted regardless, the conf file will determine if we bother looking for them.
 		cp /var/home/wavelet/config/nginx.secure.conf /var/home/wavelet/http-php/nginx/nginx.conf
 	fi
 	mkdir -p /var/home/wavelet/http-php/log
-	echo -e "
-[Pod]
-PublishPort=9080:80
-PublishPort=9443:443
-Volume=/etc/pki/tls/certs/:/etc/pki/tls/certs/
+	echo -e "[Pod]
+PublishPort=80:80
+PublishPort=443:443
+Volume=/var/home/wavelet/http-php/certs/:/etc/pki/tls/certs/:z
 Volume=/var/home/wavelet/http-php/log:/var/log/nginx:Z
 Volume=/var/home/wavelet/http-php/html:/var/www/html:Z
 Volume=/var/home/wavelet/http-php/nginx:/etc/nginx/conf.d/:z
