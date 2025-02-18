36724bfe96a90a58e1d9a2a7db7ede16296d611f
armelvil <103518994+armelvil@users.noreply.github.com>
2025-02-18 12:04:54 -0500

    Update get_uv_hash_select.php
    - now points to correct key in /UI/ prefix
    - fix header encoding

--- webfiles/root/home/wavelet/http-php/html/get_uv_hash_select.php
+++ webfiles/root/home/wavelet/http-php/html/get_uv_hash_select.php
@@ -1,16 +1,15 @@
 <?php
 header('Content-type: application/json');
-// this script curls etcd for available input devices, cleans out the cruft and re-encodes a JSON object that should be handled by the webui index.html via AJAX
+// This module gets UV hash select value, which tells the UI what the currently streaming device is.
 
 function poll_etcd_inputs($keyPrefix) {
 		$ch = curl_init();
-		// CURL example:  curl -L http://192.168.1.32:2379/v3/kv/range -X POST -d '{"key": "L2ludGVyZmFjZQ==", "range_end": "L2ludGVyZmFjZTA="}'
 		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$keyPrefix\"}");
 		$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+		$headers[] = 'Content-Type: application/json';
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 		$result = curl_exec($ch);
 		if (curl_errno($ch)) {
@@ -30,7 +29,7 @@ function poll_etcd_inputs($keyPrefix) {
 		echo $output;
 }
 
-$prefixstring = "uv_hash_select";
+$prefixstring = "/UI/UV_HASH_SELECT";
 $keyPrefix=base64_encode($prefixstring);
 poll_etcd_inputs($keyPrefix);
 ?>
