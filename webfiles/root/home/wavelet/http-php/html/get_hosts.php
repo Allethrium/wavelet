3930a02c42da4b309e30567954907f76086f0cb9
armelvil <103518994+armelvil@users.noreply.github.com>
2025-02-18 12:04:26 -0500

    Update get_hosts.php
    - fold all necessary hostdata into this PHP function so we now generate everything in one go without having to rely on async AJAX functions to come back and populate additional info.

--- webfiles/root/home/wavelet/http-php/html/get_hosts.php
+++ webfiles/root/home/wavelet/http-php/html/get_hosts.php
@@ -2,7 +2,10 @@
 header('Content-type: application/json');
 // this script curls etcd for available hosts.
 
-function poll_etcd_hosts($keyPrefix, $keyPrefixPlusOneBit) {
+function poll_etcd_hosts() {
+	$prefix					=		"/UI/hostlist";
+	$keyPrefix				=		base64_encode($prefix);
+	$keyPrefixPlusOneBit	=		base64_encode($prefix . '0');
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -16,77 +19,48 @@ function poll_etcd_hosts($keyPrefix, $keyPrefixPlusOneBit) {
 		echo 'Error:' . curl_error($ch);
 	}
 	curl_close($ch);
-
-	$dataArray = json_decode($result, true); // this decodes the JSON string as an associative array
-	$newData = [];
+	$dataArray 				=		json_decode($result, true);
 	foreach ($dataArray['kvs'] as $x => $item) {
-		$decodedKey		=	base64_decode($item['key']);
-		$decodedValue 	=	base64_decode($item['value']);
-		$hostName 		=	(str_replace ("/hostLabel/", "", (str_replace("/type", "", $decodedKey))));
-		$hostLabel		=	get_prettyname($hostName);
-		array_push($newData, [
-			'key'		=>	$decodedKey,
-			'type'		=>	$decodedValue,
-			'hostName'	=> 	(str_replace ("/hostLabel/", "", (str_replace("/type", "", $decodedKey)))),
-			'hostHash'	=>	(str_replace ("\"", "", curl_etcd($hostName))),
-			'hostLabel'	=>	$hostLabel,
-		]);
+		$key		=		str_replace ("/UI/hostlist/", "", (base64_decode($item['key'])));
+       	$value		=		base64_decode($item['value']);
+		$newData[] = [
+			'key'				=>	$key,
+			'value'				=>	$value,
+			'type'				=>	$value,
+			'hostName'			=>	$key,
+			'hostHash'			=>	curl_etcd($key . '/hash'),
+			'hostLabel'			=>	curl_etcd($key . '/control/label'),
+			'hostIP'			=>	curl_etcd($key . '/IP'),
+			'hostBlankStatus'	=>	curl_etcd($key . '/control/BLANK'),
+		];
 	}
 	$output = json_encode($newData);
 	echo $output;
 }
 
-function curl_etcd($hostName) {
-		$b64KeyTarget = base64_encode("/$hostName/Hash");
+function curl_etcd($key) {
+		$b64KeyTarget = base64_encode("/UI/hosts/$key");
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$b64KeyTarget\"}");
 		$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+		$headers[] = 'Content-Type: application/json';
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 		$hashresult = curl_exec($ch);
 		if (curl_errno($ch)) {
 				echo 'Error:' . curl_error($ch);
 		}
 		curl_close($ch);
-		$hashDataArray = json_decode($hashresult, true); // this decodes the JSON string as an associative array
+		$hashDataArray = json_decode($hashresult, true); 
 		foreach ($hashDataArray['kvs'] as $x => $item) {
 				$decodedHashKey		=	base64_decode($item['key']);
 				$decodedHashValue	=	base64_decode($item['value']);
 				$hostHashDecoded 	=	json_encode($decodedHashValue);
-		return $hostHashDecoded;
+        		return str_replace ("\"", "", $hostHashDecoded);
 		}
 }
 
-function get_prettyname($hostName){
-		$b64KeyTarget = base64_encode("/$hostName/hostNamePretty");
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$b64KeyTarget\"}");
-		$headers = array();
-		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-				echo 'Error:' . curl_error($ch);
-		}
-		curl_close($ch);
-	$dataArray = json_decode($result, true);
-		foreach ($dataArray['kvs'] as $x => $item) {
-				$decodedKey = base64_decode($item['key']);
-				$decodedValue = base64_decode($item['value']);
-		}
-		return $decodedValue;
-}
-
-// modify these strings if you want to get a different key prefix out of etcd.  For Wavelet, there's no reason to change them.
-$prefixstring = '/hostLabel';
-$prefixstringplusone = '/hostLabel0';
-$keyPrefix=base64_encode($prefixstring);
-$keyPrefixPlusOneBit=base64_encode($prefixstringplusone);
-poll_etcd_hosts($keyPrefix, $keyPrefixPlusOneBit);
+poll_etcd_hosts();
 ?>
