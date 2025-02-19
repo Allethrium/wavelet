f257e5905ff9f4caa7452c84ef56e9cc9ccab79c
armelvil <103518994+armelvil@users.noreply.github.com>
2025-02-19 15:17:49 -0500

    Update wavelet_revert_server.sh

--- webfiles/root/usr/local/sbin/wavelet_revert_server.sh
+++ webfiles/root/usr/local/sbin/wavelet_revert_server.sh
@@ -4,7 +4,7 @@
 # destroys the etcd cluster data,
 # and reverts the server back to a "mint" condition before the build_ug module performs server bootstrap.
 # Must be run as root
-
+systemctl stop etcd-quadlet.service
 /usr/bin/rm -rf /var/lib/etcd-data
 /usr/bin/rm -rf /var/home/wavelet/{server_bootstrap_completed,encoder.firstrun,reflector_clients_ip.txt}
 /usr/bin/systemctl reboot -i
