92814289e5010343d8624bd8e488f6cd48f258d2
armelvil <103518994+armelvil@users.noreply.github.com>
2025-03-07 16:11:25 -0500

    - web auth working
    - etcd via curl auth now functional
    - need to clean up keys and ensure they are populated in the right places

--- webfiles/root/home/wavelet/http-php/html/get_auth_token.php
+++ webfiles/root/home/wavelet/http-php/html/get_auth_token.php
@@ -1,31 +1,45 @@
 <?php
 // Grabs an auth token based on the password string set during server spinup in NGINX config
 // These vars shouldn't be accessible from the web browser side, and even if they are, they grant access only to /UI/
-function get_etcd_authtoken($password, $username) {
-	$password = base64_encode($_ENV['PASSWORD']);
-	$username = base64_encode('webui');
+
+function get_etcd_authtoken($username, $password) {
+	$data = [
+		'name' => $username,
+		'password'=> str_replace("\n", "", $password)];
+	$post_data = json_encode($data);
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-	// returns etcd authorization token utilizing the systemd password variable and the username webui
-	curl_setopt($ch, CURLOPT_URL, 'http://1982.168.1.32:2379/v3/auth/authenticate');
+	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/auth/authenticate');
 	$headers = [
 		'Content-Type: application/json',
 	];
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"name\": \"$username\", \"password\": \"$password\"}");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	$token = curl_exec($ch);
+	$response = curl_exec($ch);
+	//$response =curl_getinfo($ch);
 	curl_close($ch);
-	$dataArray = json_decode($result, true);
-	foreach ($dataArray['kvs'] as $x => $item) {
-			$decodedKey = base64_decode($item['key']);
-			$decodedValue = base64_decode($item['value']);
-	}
-	return $decodedValue;
+	$responseArray = json_decode($response, true);
+	$token = $responseArray['token'];
+	return $token;
 }
-$password = $_ENV['PASSWORD'];
-$username = 'webui';
-$auth_token=get_etcd_authtoken($password, $username);
-return $auth_token;
+
+function base64_url_decode($input) {
+ return strtr($input, '._-', '+/+');
+}
+
+function decrypt($pw2) {
+	//return openssl_decrypt(base64_decode(file_get_contents("/var/secrets/crypt.bin")), 'aes-256-cbc', $key, OPENSSL_RAW_DATA);
+	return base64_decode((shell_exec("openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -in /var/secrets/crypt.bin -nosalt -pass pass:$pw2")));
+}
+
+function get_etcd_auth_token() {
+$dir			= 	"/var/secrets";
+$username		= 	"webui";
+$pw2 			=	file_get_contents("$dir/pw2.txt");
+$password		=	decrypt($pw2);
+$token			=	get_etcd_authtoken($username, $password);
+return $token;
+}
 ?>
--- webfiles/root/home/wavelet/http-php/html/get_hosts.php
+++ webfiles/root/home/wavelet/http-php/html/get_hosts.php
@@ -1,18 +1,19 @@
 <?php
 header('Content-type: application/json');
-include('get_auth_token.php');
+include 'get_auth_token.php';
 // this script curls etcd for available hosts.
 
-function poll_etcd_hosts() {
+function poll_etcd_hosts($token) {
 	$prefix					=		"/UI/hostlist";
 	$keyPrefix				=		base64_encode($prefix);
-	$keyPrefixPlusOneBit	=		base64_encode($prefix . '0');
+	$keyPrefixPlusOneBit			=		base64_encode($prefix . '0');
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\": \"$keyPrefix\", \"range_end\": \"$keyPrefixPlusOneBit\"}");
 	$headers = array();
+	$headers[] = 'Authorization: ' .  $token;
 	$headers[] = 'Content-Type: application/x-www-form-urlencoded';
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 	$result = curl_exec($ch);
@@ -22,50 +23,49 @@ function poll_etcd_hosts() {
 	curl_close($ch);
 	$dataArray 				=		json_decode($result, true);
 	foreach ($dataArray['kvs'] as $x => $item) {
-		$key		=		str_replace ("/UI/hostlist/", "", (base64_decode($item['key'])));
-       	$value		=		base64_decode($item['value']);
+		$key	=		str_replace ("/UI/hostlist/", "", (base64_decode($item['key'])));
+		$value	=		base64_decode($item['value']);
 		$newData[] = [
 			'key'				=>	$key,
 			'value'				=>	$value,
 			'type'				=>	$value,
 			'hostName'			=>	$key,
-			'hostHash'			=>	curl_etcd($key . '/hash'),
-			'hostLabel'			=>	curl_etcd($key . '/control/label'),
-			'hostIP'			=>	curl_etcd($key . '/IP'),
-			'hostBlankStatus'	=>	curl_etcd($key . '/control/BLANK'),
+			'hostHash'			=>	curl_etcd($key . '/hash', $token),
+			'hostLabel'			=>	curl_etcd($key . '/control/label', $token ),
+			'hostIP'			=>	curl_etcd($key . '/IP', $token ),
+			'hostBlankStatus'		=>	curl_etcd($key . '/control/BLANK', $token)
 		];
 	}
 	$output = json_encode($newData);
 	echo $output;
 }
 
-function curl_etcd($key) {
-		$b64KeyTarget = base64_encode("/UI/hosts/$key");
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$b64KeyTarget\"}");
+function curl_etcd($key, $token) {
+	//echo nl2br ("Attempting to get: /UI/hosts/$key\n");
+	$b64KeyTarget = base64_encode("/UI/hosts/$key");
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$b64KeyTarget\"}");
 		$headers = array();
-		$headers = [
-			"Authorization: $token",
-			"Content-Type: application/json"
-		];
+		$headers[] = 'Authorization: ' .  $token;
+		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$hashresult = curl_exec($ch);
+		$result = curl_exec($ch);
 		if (curl_errno($ch)) {
-				echo 'Error:' . curl_error($ch);
+			echo 'Error:' . curl_error($ch);
 		}
 		curl_close($ch);
-		$hashDataArray = json_decode($hashresult, true); 
-		foreach ($hashDataArray['kvs'] as $x => $item) {
-				$decodedHashKey		=	base64_decode($item['key']);
-				$decodedHashValue	=	base64_decode($item['value']);
-				$hostHashDecoded 	=	json_encode($decodedHashValue);
-        		return str_replace ("\"", "", $hostHashDecoded);
-		}
+	$hashDataArray = json_decode($result, true); 
+	foreach ($hashDataArray['kvs'] as $x => $item) {
+		$decodedHashKey		=	base64_decode($item['key']);
+		$decodedHashValue	=	base64_decode($item['value']);
+		$hostHashDecoded 	=	json_encode($decodedHashValue);
+		return str_replace ("\"", "", $hostHashDecoded);
+	}
 }
 
-$token=get_etcd_authtoken;
-poll_etcd_hosts();
+$token	= get_etcd_auth_token();
+poll_etcd_hosts($token);
 ?>
