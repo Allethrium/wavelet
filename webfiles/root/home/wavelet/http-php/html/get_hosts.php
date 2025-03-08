2c4b215d97b824722e36912fda9305795f3fd1f0
armelvil <103518994+armelvil@users.noreply.github.com>
2025-03-08 11:18:51 -0500

    Update get_hosts.php
    - git, rly?

--- webfiles/root/home/wavelet/http-php/html/get_hosts.php
+++ webfiles/root/home/wavelet/http-php/html/get_hosts.php
@@ -1,16 +1,13 @@
 <?php
 header('Content-type: application/json');
 include 'get_auth_token.php';
+
 // this script curls etcd for available hosts.
 
 function poll_etcd_hosts($token) {
 	$prefix					=		"/UI/hostlist";
 	$keyPrefix				=		base64_encode($prefix);
-<<<<<<< Updated upstream
-	$keyPrefixPlusOneBit			=		base64_encode($prefix . '0');
-=======
 	$keyPrefixPlusOneBit	=		base64_encode($prefix . '0');
->>>>>>> Stashed changes
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, 'http://192.168.1.32:2379/v3/kv/range');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -27,15 +24,9 @@ function poll_etcd_hosts($token) {
 	curl_close($ch);
 	$dataArray 				=		json_decode($result, true);
 	foreach ($dataArray['kvs'] as $x => $item) {
-<<<<<<< Updated upstream
 		$key	=		str_replace ("/UI/hostlist/", "", (base64_decode($item['key'])));
 		$value	=		base64_decode($item['value']);
 		$newData[] = [
-=======
-		$key				=		str_replace ("/UI/hostlist/", "", (base64_decode($item['key'])));
-		$value				=		base64_decode($item['value']);
-		$newData[] 			= [
->>>>>>> Stashed changes
 			'key'				=>	$key,
 			'value'				=>	$value,
 			'type'				=>	$value,
@@ -46,7 +37,7 @@ function poll_etcd_hosts($token) {
 			'hostBlankStatus'		=>	curl_etcd($key . '/control/BLANK', $token)
 		];
 	}
-	$output 				= 		json_encode($newData);
+	$output = json_encode($newData);
 	echo $output;
 }
 
@@ -58,7 +49,6 @@ function curl_etcd($key, $token) {
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"key\":\"$b64KeyTarget\"}");
-<<<<<<< Updated upstream
 		$headers = array();
 		$headers[] = 'Authorization: ' .  $token;
 		$headers[] = 'Content-Type: application/x-www-form-urlencoded';
@@ -73,32 +63,11 @@ function curl_etcd($key, $token) {
 		$decodedHashKey		=	base64_decode($item['key']);
 		$decodedHashValue	=	base64_decode($item['value']);
 		$hostHashDecoded 	=	json_encode($decodedHashValue);
-=======
-	$headers = array();
-	$headers[] = 'Authorization: ' .  $token;
-	$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	$result = curl_exec($ch);
-	if (curl_errno($ch)) {
-		echo 'Error:' . curl_error($ch);
-	}
-	curl_close($ch);
-	$hashDataArray			=	json_decode($result, true); 
-	foreach ($hashDataArray['kvs'] as $x => $item) {
-		$decodedHashKey		=	base64_decode($item['key']);
-		$decodedHashValue	=	base64_decode($item['value']);
-		$hostHashDecoded	=	json_encode($decodedHashValue);
->>>>>>> Stashed changes
 		return str_replace ("\"", "", $hostHashDecoded);
 	}
 }
 
-<<<<<<< Updated upstream
 $token	= get_etcd_auth_token();
+
 poll_etcd_hosts($token);
 ?>
-=======
-$token						=	get_etcd_auth_token();
-poll_etcd_hosts($token);
-?>
->>>>>>> Stashed changes
