52183ad455ee55597ab4972d0d89226ee615510d
armelvil <103518994+armelvil@users.noreply.github.com>
2025-03-12 12:55:16 -0400

    Update wavelet_provision.sh
    - provision flow PoC

--- webfiles/root/usr/local/bin/wavelet_provision.sh
+++ webfiles/root/usr/local/bin/wavelet_provision.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 # Calls etcd_interaction in order to provision a client
+# This is the only service that runs from wavelet-root, because it needs root privs to the etcd cluster
+# I didn't want those to be accessible from the wavelet user in an effort to be more secure, nor did I want this stuff running as root.
 
-# Do some security checking and screening, discard if it isn't REQUEST-hostname pattern
-# screen for binary data, html, regex, other possible active content and discard.
+if [ "$EUID" -ne 9337 ]
+	then echo "This module should not only run under wavelet-root."
+	exit 1
+fi
 
-# check hostname w/ DNS
-verifiedHostName=$(whatever comes out of here)
-requestingClient=${verifiedHostName}
-# call etcd interaction
+KEYNAME="/PROV/REQUEST"; /usr/local/bin/wavelet_etcd_interaction.sh "read_etcd_global" "${KEYNAME}"
 
-/usr/local/bin/wavelet_etcd_interaction.sh "generate_etcd_host_role" 0 ${requestingClient}
+if [[ ${printvalue} == *"$(dnsdomainname)" ]]; then
+	echo "Client request domain name correct, proceeding"
+else
+	echo "Domain name incorrect for client machine, exiting."
+	exit 1
+fi
+
+if [[ ${printvalue} == "*svr*" ]]; then
+	echo "Provision request cannot be a server!"
+	exit 1
+fi
+
+# call etcd interaction to generate the host role
+/usr/local/bin/wavelet_etcd_interaction.sh "generate_etcd_host_role"
