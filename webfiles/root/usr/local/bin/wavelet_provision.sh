811cf7547d6fa4d5a7aa7f716f04e81a0156eb40
armelvil <103518994+armelvil@users.noreply.github.com>
2025-03-13 16:08:19 -0400

    Update wavelet_provision.sh
    - two step provision module designed to be called by two different uids for a more "secure" process

--- webfiles/root/usr/local/bin/wavelet_provision.sh
+++ webfiles/root/usr/local/bin/wavelet_provision.sh
@@ -3,24 +3,48 @@
 # This is the only service that runs from wavelet-root, because it needs root privs to the etcd cluster
 # I didn't want those to be accessible from the wavelet user in an effort to be more secure, nor did I want this stuff running as root.
 
-if [ "$EUID" -ne 9337 ]
-	then echo "This module should not only run under wavelet-root."
+step1() {
+	if [[ "$EUID" -ne 9337 ]]; then 
+		echo "This step should only run under wavelet-root."
+		exit 1
+	fi
+	KEYNAME="/PROV/REQUEST"; printvalue=$(/usr/local/bin/wavelet_etcd_interaction.sh "read_etcd_global" "${KEYNAME}")
+	if [[ ${printvalue} == *"$(dnsdomainname)" ]]; then
+		echo "Client request domain name correct, proceeding"
+	else
+		echo "Domain name incorrect for client machine, exiting."
 	exit 1
-fi
+	fi
+	if [[ ${printvalue} == "*svr*" ]]; then
+		echo "Provision request cannot be a server!"
+		exit 1
+	fi
+	# call etcd interaction to generate the host role
+	/usr/local/bin/wavelet_etcd_interaction.sh "generate_etcd_host_role"
+}
 
-KEYNAME="/PROV/REQUEST"; /usr/local/bin/wavelet_etcd_interaction.sh "read_etcd_global" "${KEYNAME}"
+step2() {
+	if [[ "$EUID" -ne 1337 ]]; then 
+		echo "This step should only run as the wavelet user on the client machine"
+		exit 1
+	fi
+	/usr/local/bin/wavelet_etcd_interaction.sh "client_provision_data"
+}
 
-if [[ ${printvalue} == *"$(dnsdomainname)" ]]; then
-	echo "Client request domain name correct, proceeding"
+
+#####
+#
+# Main
+#
+#####
+
+#set -x
+exec > $HOME/logs/provision_request.log 2>&1
+inputargs=$@
+
+if [[ $@ = "2" ]]; then
+	echo "step 2 provisioning activated"
+	step2
 else
-	echo "Domain name incorrect for client machine, exiting."
-	exit 1
+	step1
 fi
-
-if [[ ${printvalue} == "*svr*" ]]; then
-	echo "Provision request cannot be a server!"
-	exit 1
-fi
-
-# call etcd interaction to generate the host role
-/usr/local/bin/wavelet_etcd_interaction.sh "generate_etcd_host_role"
